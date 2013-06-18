01ddaadc742ff28108dc0d0466674e191b0be6b2
Kaioshi <waderr at gmail dot com>
2013-06-18 21:26:38 +0800

    Added ;memstats

---
+++ plugins/memstats.js
@@ -0,0 +1,13 @@
+﻿// Memory usage report
+listen({
+    handle: "memstats",
+    regex: regexFactory.startsWith("memstats"),
+    command: {
+        root: "memstats",
+        options: "No options",
+        help: "Shows memory usage."
+    },
+    callback: function (input) {
+        irc.say(input.context, input.from + ": I'm currently using " + Funcs.memUse() + " MiB of memory.");
+    }
+});
