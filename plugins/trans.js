1b63c1672ae2c9b6acc2345141768c932c25fc43
Kaioshi <waderr at gmail dot com>
2013-06-29 05:29:15 +0800

    add dodgy translator

---
+++ plugins/trans.js
@@ -0,0 +1,44 @@
+//0cb48
+listen({
+	handle: "trans",
+	regex: regexFactory.startsWith(["trans", "translate"]),
+	command: {
+		root: "trans",
+		options: "[-d dict] <word/term>",
+		help: "attempts to translate from language to language. Example: trans -d enfr pants - see http://www.wordreference.com/docs/api.aspx for a list of dictionaries."
+	},
+	callback: function (input) {
+		if (!config.api.wordreference) {
+			irc.say(input.context, "sorry, the wordreference API key is missing from config.");
+			return;
+		}
+		var args = input.match[1].split(" "), uri, dict, term, result;
+		if (args[0]) {
+			if (args[0] === "-d" && args[1] && args[2]) {
+				dict = args[1];
+				term = args.slice(2).join(" ");
+			} else {
+				dict = "enja";
+				term = args.join(" ");
+			}
+			uri = "http://api.wordreference.com/0.8/"+config.api.wordreference+"/json/"+dict+"/"+term;
+			web.get(uri, function (error, response, body) {
+				var result = JSON.parse(body);
+				if (result.Error) {
+					irc.say(input.context, result.Note.split('\n')[0]);
+				} else {
+					if (result.term0) var res = result.term0;
+					else if (result.term1) var res = result.term1;
+					if (res) {
+						if (res.Entries) var tr = res.Entries["0"].FirstTranslation["term"];
+						else if (res.PrincipalTranslations) var tr = res.PrincipalTranslations["0"].FirstTranslation["term"];
+					}
+					if (tr) irc.say(input.context, "(en) "+term+" -> ("+dict.slice(2)+") "+tr, false);
+					else irc.say(input.context, "Something went wrong.");
+				}
+			});
+		} else {
+			irc.say(input.context, "[Help] Syntax: trans " + this.command.options);
+		}
+	}
+});
