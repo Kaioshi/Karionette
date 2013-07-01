876d61022fa9bf5deb0b485cb59c94a6970f084f
Kaioshi <waderr at gmail dot com>
2013-07-02 01:11:20 +0800

    fix <title> regex a bit

--- plugins/titlesnarfer.js
+++ plugins/titlesnarfer.js
@@ -5,7 +5,7 @@ listen({
 	handle: "titleSnarfer",
 	regex: new RegExp("^:[^ ]+ PRIVMSG [^ ]+ :?.*((?:https?:\\/\\/)[^ ]+)"),
 	callback: function (input, match) {
-		var result, title, host,
+		var result, reg, host,
 			uri = match[1],
 			ext = uri.split("/"),
 			ext = ext[ext.length-1].split(".")[1],
@@ -14,21 +14,11 @@ listen({
 		web.get(uri, function (error, response, body) {
 			if (error) logger.error("titleSnarfer: "+error);
 			else {
-				title = getTitle(body);
-				if (title) {
-					title = ent.decode(title);
-					irc.say(input.context, title.trim() + " ~ " + response.request.host);
-				}
+				reg = (/(<title?.*>)(.+?)(<\/title>)/ig).exec(body);
+				if (reg) irc.say(input.context, ent.decode(reg[2]).trim() + " ~ " + response.request.host);
 				else logger.warn("titleSnarfer: couldn't get title for "+uri);
 			}
 		});
 	}
 });
 
-function getTitle(html) {
-	return (html.replace( /<!\[CDATA\[(.+?)\]\]>/g, function (_match, body) {
-		return body.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
-	}).replace(/<!--.+?-->/g, '').match(/<title>.+?<\/title>/ig) || []).map(function (t) {
-		return t.substring(7, t.length - 8);
-	}).join(" ");
-}
