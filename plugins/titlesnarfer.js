6cdd7714cd7478bffad31254e1df649c8d9c6ceb
Kaioshi <waderr at gmail dot com>
2013-07-07 16:41:38 +0800

    [titleSnarfer] fix youtube and imgur urls not working sometimes

--- plugins/titlesnarfer.js
+++ plugins/titlesnarfer.js
@@ -13,7 +13,8 @@ listen({
 			tmpwget = "",
 			ext = /.*\.([a-zA-Z0-9]+)$/.exec(uri.path),
 			allow = [ 'htm', 'html', 'asp', 'aspx', 'php', 'php3', 'php5' ];
-		if (ext) {
+		if (ext && ext[0].indexOf('?') === -1) {
+			logger.debug("ext[0]: "+ext[0]);
 			logger.debug("Checking if file extension is OK.");
 			if (!allow.some(function (item) { return (ext[1] === item); })) {
 				logger.debug("Rejected ext: "+ext[1]);
@@ -21,11 +22,16 @@ listen({
 			}
 		}
 		tmpwget = "data/.tmp.wget."+Math.floor(Math.random()*8175).toString();
-		sys.exec("wget -q -O - "+uri.href+" | head -c 5000 | egrep \\<title?.*\\>\\(.*?\\)\\</title\\> > "+tmpwget);
+		sys.exec("wget -q -O - "+uri.href.replace(/&/g, "\\&")+" | head -c 5000 | egrep \\<title?.*\\>\\(.*?\\) > "+tmpwget);
 		setTimeout(function () {
-			var title = fs.readFileSync(tmpwget).toString();
+			var title = fs.readFileSync(tmpwget).toString(),
+				reg;
 			if (title) {
-				var reg = /(<title?.*>)(.+?)(<\/title>)/ig.exec(title);
+				if (title.indexOf("</title>") > -1) {
+					reg = /(<title?.*>)(.*?)(<\/title>)/ig.exec(title);
+				} else { // imgur is weird.
+					reg = /(<title?.*>)(.*)/ig.exec(title);
+				}
 				if (reg) irc.say(input.context, ent.decode(reg[2].trim()) + " ~ " + uri.host);
 				else logger.debug("No title found in the first 5000 bytes of "+uri.href);
 				reg = null;
