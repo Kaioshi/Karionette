49434176731d26ac784c1804f87da13ac7cdb3cb
Kaioshi <waderr at gmail dot com>
2013-07-15 10:03:01 +0800

    remove newlines only when needed.

--- plugins/ud.js
+++ plugins/ud.js
@@ -13,7 +13,9 @@ listen({
 			uri = 'http://api.urbandictionary.com/v0/define?term=' + match[1];
 		web.get(uri, function (error, response, body) {
 			result = JSON.parse(body).list;
-			result[0].definition = result[0].definition.replace(/\n/g, " ");
+			if (result[0].definition.indexOf('\n') > -1) {
+				result[0].definition = result[0].definition.replace(/\n/g, " ");
+			}
 			if (result[0]) {
 				irc.say(input.context, result[0].definition, false);
 			} else {
