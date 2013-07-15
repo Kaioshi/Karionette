d0b06957736a3dfdd6fe27ff92bf99de7f3b4a69
Kaioshi <waderr at gmail dot com>
2013-07-15 10:00:15 +0800

    [ud] remove newlines in definition

--- plugins/ud.js
+++ plugins/ud.js
@@ -13,6 +13,7 @@ listen({
 			uri = 'http://api.urbandictionary.com/v0/define?term=' + match[1];
 		web.get(uri, function (error, response, body) {
 			result = JSON.parse(body).list;
+			result[0].definition = result[0].definition.replace(/\n/g, " ");
 			if (result[0]) {
 				irc.say(input.context, result[0].definition, false);
 			} else {
