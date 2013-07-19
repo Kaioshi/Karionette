7c4d7b80a45eed18992faca1a6768cac5bdc9ccb
Kaioshi <waderr at gmail dot com>
2013-07-19 21:13:08 +0800

    [titleSnarfer] fix kotaku links

--- plugins/titlesnarfer.js
+++ plugins/titlesnarfer.js
@@ -9,7 +9,7 @@ listen({
 	handle: "titleSnarfer",
 	regex: new RegExp("^:[^ ]+ PRIVMSG [^ ]+ :?.*((?:https?:\\/\\/)[^ ]+)"),
 	callback: function (input, match) {
-		var uri, title, reg, tmpwget, ext, allow;
+		var uri, title, reg, tmpwget, ext, allow, length = 5000;
 		if (input.data[0] === config.command_prefix) return;
 		uri = url.parse(match[1]);
 		tmpwget = "";
@@ -21,7 +21,10 @@ listen({
 			}
 		}
 		tmpwget = "data/.tmp.wget."+Math.floor(Math.random()*8175).toString();
-		sys.exec("wget -q -O - "+uri.href.replace(/&/g, "\\&")+" | head -c 5000 | egrep \\<title?.*\\>\\(.*?\\) > "+tmpwget);
+		// ton of garbage in the first 15000 characters. o_O
+		if (uri.host.indexOf("kotaku") > -1) length = 20000;
+		
+		sys.exec("wget -q -O - "+uri.href.replace(/&/g, "\\&")+" | head -c "+length+" | grep -E -io \\<title?.*\\>\\(.*?\\)\\(\\<\\/title\\>\\)? > "+tmpwget);
 		setTimeout(function () {
 			title = fs.readFileSync(tmpwget).toString();
 			if (title) {
@@ -32,7 +35,7 @@ listen({
 				}
 				reg[2] = ent.decode(reg[2].trim());
 				if (reg && reg[2].length > 0) irc.say(input.context, reg[2] + " ~ " + uri.host);
-				else logger.debug("No title found in the first 5000 bytes of "+uri.href);
+				else logger.debug("No title found in the first "+length+" bytes of "+uri.href);
 				reg = null;
 				title = null;
 			}
