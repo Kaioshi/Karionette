29ffcb5db11870f9fad8945f8d8ff10682b21abc
Kaioshi <waderr at gmail dot com>
2013-07-26 10:42:16 +0800

    [titleSnarfer] don't use a temp file.

--- plugins/titlesnarfer.js
+++ plugins/titlesnarfer.js
@@ -1,15 +1,14 @@
 // url title snarfer - THIS ONLY WORKS ON UNIX - must have wget, head and egrep installed.
 var ent = require("./lib/entities.js"),
 	sys = require("sys"),
-	url = require("url"),
-	fs = require("fs");
+	url = require("url");
 
 listen({
 	plugin: "titleSnarfer",
 	handle: "titleSnarfer",
 	regex: new RegExp("^:[^ ]+ PRIVMSG [^ ]+ :?.*((?:https?:\\/\\/)[^ ]+)"),
 	callback: function (input, match) {
-		var uri, title, reg, tmpwget, ext, allow, length = 5000;
+		var uri, title, reg, ext, allow, length = 5000;
 		
 		function zero(n) {
 			return (n > 9 ? n : "0"+n);
@@ -42,7 +41,6 @@ listen({
 			youtubeIt(/^\/([^ &]+)/.exec(uri.path)[1], uri.host);
 			return;
 		}
-		tmpwget = "";
 		ext = /.*\.([a-zA-Z0-9]+)$/.exec(uri.path);
 		allow = [ 'htm', 'html', 'asp', 'aspx', 'php', 'php3', 'php5' ];
 		if (ext && !ext[0].match(/&|\?/)) {
@@ -50,30 +48,19 @@ listen({
 				return;
 			}
 		}
-		tmpwget = "data/.tmp.wget."+Math.floor(Math.random()*8175).toString();
 		// ton of garbage in the first 15000 characters. o_O
 		if (uri.host.indexOf("kotaku") > -1) length = 20000;
-		
-		sys.exec("wget -q -O - "+uri.href.replace(/&/g, "\\&")+" | head -c "+length+" | grep -E -io \\<title?.*\\>\\(.*?\\)\\(\\<\\/title\\>\\)? > "+tmpwget);
-		setTimeout(function () {
-			if (fs.lstatSync(tmpwget).size > 0) {
-				title = fs.readFileSync(tmpwget).toString();
-				if (title.indexOf("</title>") > -1) {
-					reg = /(<title?.*>)(.*?)(<\/title>)/ig.exec(title);
-				} else {
-					reg = /(<title?.*>)(.*)/ig.exec(title);
-				}
-				reg[2] = ent.decode(reg[2].trim());
-				if (reg && reg[2].length > 0) {
-					irc.say(input.context, reg[2] + " ~ " + uri.host);
-				} else {
-					logger.debug("No title found in the first "+length+" bytes of "+uri.href);
-				}
-				reg = null;
-				title = null;
+		sys.exec("wget -q -O- "+uri.href.replace(/&/g, "\\&")+" | head -c "+length+
+			" | tr \'\\n\' \' \' | grep -E -io \"<title?.*>(.*?)<\/title>\" | grep -E -o \">(.*)<\"",
+		function (error, stdout, stderr) {
+			title = stdout.slice(1,-2).trim();
+			if (title.slice(-8) === " - Imgur") title = title.slice(0,-8);
+			if (title) {
+				irc.say(input.context, title+" ~ "+uri.host);
+			} else {
+				logger.debug("Couldn't find title in first "+length+" bytes of "+uri.href);
 			}
-			fs.unlink(tmpwget);
-		}, 2000);
+		});
 	}
 });
 
