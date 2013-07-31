5fdf65d78f18d9b09d2d8cc2cd15692604258692
Kaioshi <waderr at gmail dot com>
2013-07-31 11:16:17 +0800

    [permissions] must be used via query

--- plugins/permissions.js
+++ plugins/permissions.js
@@ -10,98 +10,98 @@ listen({
 	},
 	callback: function (input, match) {
 		var user, access, args = match[1].split(" ");
-		if (args && args.length > 0) {
-			switch (args[0]) {
-				case "allow":
-					switch (args[1]) {
-						case "add":
-							irc.say(input.context, permissions.Allow.Add(input.user, args[2], args[3], args[4]));
-							break;
-						case "remove":
-							irc.say(input.context, permissions.Allow.Remove(input.user, args[2], args[3], args[4]));
-							break;
-						case "list":
-							irc.say(input.context, permissions.Allow.List(args[2], args[3]));
-							break;
-						default:
-							irc.say(input.context, permissions.Syntax + " - " + permissions.Example);
-							break;
-					}
-					break;
-				case "deny":
-					switch (args[1]) {
-						case "add":
-							irc.say(input.context, permissions.Deny.Add(input.user, args[2], args[3], args[4]));
-							break;
-						case "remove":
-							irc.say(input.context, permissions.Deny.Remove(input.user, args[2], args[3], args[4]));
-							break;
-						case "list":
-							irc.say(input.context, permissions.Deny.List(args[2], args[3]));
-							break;
-						default:
-							irc.say(input.context, permissions.Syntax + " - " + permissions.Example);
-							break;
-					}
-					break;
-				case "owner":
-					switch (args[1]) {
-						case "add":
-							irc.say(input.context, permissions.Owner.Add(input.user, args[2], args[3], args[4]));
-							break;
-						case "remove":
-							irc.say(input.context, permissions.Owner.Remove(input.user, args[2], args[3], args[4]));
-							break;
-						case "list":
-							irc.say(input.context, permissions.Owner.List(input.user, args[2], args[3]));
-							break;
-						default:
-							irc.say(input.context, permissions.Syntax + " - " + permissions.Example);
-							break;
-					}
-					break;
-				case "admin":
-					switch (args[1]) {
-						case "add":
-							irc.say(input.context, permissions.Admin.Add(input.user, args[2], args[3]));
-							break;
-						case "remove":
-							irc.say(input.context, permissions.Admin.Remove(input.user, args[2], args[3]));
-							break;
-						case "list":
-							irc.say(input.context, permissions.Admin.List(input.user, args[2]));
-							break;
-						default:
-							irc.say(input.context, permissions.Syntax + " - " + permissions.Example);
-							break;
-					}
-					break;
-				case "check":
-					if (args[2]) {
-						user = args[3] || input.user;
-						access = permissions.Check(args[1], args[2], user);
-						if (access) irc.say(input.context, "Allowed! :D");
-						else irc.say(input.context, "Denied! >:(");
-					} else {
-						irc.say(input.context, "[Help] Syntax: permissions check <function/alias/variable> <name> [<user>]");
-						irc.say(input.context, "Example: permissions check variable anime_list mitch!mitchy@mitch.com");
-					}
-					break;
-				case "search":
-					if (args[1]) {
-						permissions.Search(args[1]);
-					} else {
-						irc.say(input.context, "[Help] Syntax: permissions search <name>");
-					}
-					break;
-				case "list":
-					
-				default:
-					irc.say(input.context, permissions.Syntax + " - " + permissions.Example);
-					break;
-			}
-		} else {
-			irc.say(input.context, "[Help] Syntax: permissions " + this.command.options);
+		if (input.context[0] === "#") {
+			irc.notice(input.from, "permissions can only be used via query.");
+			return;
+		}
+		switch (args[0]) {
+			case "allow":
+				switch (args[1]) {
+					case "add":
+						irc.say(input.context, permissions.Allow.Add(input.user, args[2], args[3], args[4]));
+						break;
+					case "remove":
+						irc.say(input.context, permissions.Allow.Remove(input.user, args[2], args[3], args[4]));
+						break;
+					case "list":
+						irc.say(input.context, permissions.Allow.List(args[2], args[3]));
+						break;
+					default:
+						irc.say(input.context, permissions.Syntax + " - " + permissions.Example);
+						break;
+				}
+				break;
+			case "deny":
+				switch (args[1]) {
+					case "add":
+						irc.say(input.context, permissions.Deny.Add(input.user, args[2], args[3], args[4]));
+						break;
+					case "remove":
+						irc.say(input.context, permissions.Deny.Remove(input.user, args[2], args[3], args[4]));
+						break;
+					case "list":
+						irc.say(input.context, permissions.Deny.List(args[2], args[3]));
+						break;
+					default:
+						irc.say(input.context, permissions.Syntax + " - " + permissions.Example);
+						break;
+				}
+				break;
+			case "owner":
+				switch (args[1]) {
+					case "add":
+						irc.say(input.context, permissions.Owner.Add(input.user, args[2], args[3], args[4]));
+						break;
+					case "remove":
+						irc.say(input.context, permissions.Owner.Remove(input.user, args[2], args[3], args[4]));
+						break;
+					case "list":
+						irc.say(input.context, permissions.Owner.List(input.user, args[2], args[3]));
+						break;
+					default:
+						irc.say(input.context, permissions.Syntax + " - " + permissions.Example);
+						break;
+				}
+				break;
+			case "admin":
+				switch (args[1]) {
+					case "add":
+						irc.say(input.context, permissions.Admin.Add(input.user, args[2], args[3]));
+						break;
+					case "remove":
+						irc.say(input.context, permissions.Admin.Remove(input.user, args[2], args[3]));
+						break;
+					case "list":
+						irc.say(input.context, permissions.Admin.List(input.user, args[2]));
+						break;
+					default:
+						irc.say(input.context, permissions.Syntax + " - " + permissions.Example);
+						break;
+				}
+				break;
+			case "check":
+				if (args[2]) {
+					user = args[3] || input.user;
+					access = permissions.Check(args[1], args[2], user);
+					if (access) irc.say(input.context, "Allowed! :D");
+					else irc.say(input.context, "Denied! >:(");
+				} else {
+					irc.say(input.context, "[Help] Syntax: permissions check <function/alias/variable> <name> [<user>]");
+					irc.say(input.context, "Example: permissions check variable anime_list mitch!mitchy@mitch.com");
+				}
+				break;
+			case "search":
+				if (args[1]) {
+					permissions.Search(args[1]);
+				} else {
+					irc.say(input.context, "[Help] Syntax: permissions search <name>");
+				}
+				break;
+			case "list":
+				
+			default:
+				irc.say(input.context, permissions.Syntax + " - " + permissions.Example);
+				break;
 		}
 	}
 });
