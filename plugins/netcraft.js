22fc43d82fcb915fbeaa03572c76d3a82c88ccf8
Kaioshi <waderr at gmail dot com>
2013-08-09 04:53:58 +0800

    add ;netcraft

---
+++ plugins/netcraft.js
@@ -0,0 +1,23 @@
+listen({
+	plugin: "netcraft",
+	handle: "netcraft",
+	regex: regexFactory.startsWith("netcraft"),
+	command: {
+	
+	},
+	callback: function (input, match) {
+		var uri, reg, resp;
+		if (!match[1].match(/https?:\/\/[^ ]+\.[^ ]+/i)) {
+			irc.say(input.context, config.command_prefix+"netcraft http://www.pantsu.org");
+			return;
+		}
+		uri = "http://mirror.toolbar.netcraft.com/check_url/"+match[1];
+		web.get(uri, function (error, response, body) {
+			reg = /(.*) (Rank:.*) Site Report (.*)$/i.exec(lib.stripHtml(body));
+			resp = [ reg[1] ];
+			if (reg[2].slice(-1) !== "-") resp.push(reg[2]);
+			resp.push(reg[3]);
+			irc.say(input.context, resp.join(", "));
+		});
+	}
+});
