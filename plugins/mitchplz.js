fc12745cbe9b2f000ed78f274b3fd0ff9314a7c8
Kaioshi <waderr at gmail dot com>
2013-08-09 00:00:53 +0800

    [mitchplz] add more responses

--- plugins/mitchplz.js
+++ plugins/mitchplz.js
@@ -1,29 +1,48 @@
+// Mitchu harrassment 2013
+
+function randReply(yarr) {
+	return yarr[Math.floor(Math.random()*yarr.length)];
+}
+
 listen({
 	plugin: "mitchplz",
 	handle: "mitchplz",
 	regex: /^:mitch_![^ ]+ PRIVMSG #[^ ]+ :(.*)$/,
 	callback: function (input, match) {
-		var args = match[1].split(" "),
-			gfresp = [
-				"<mitch_> GURLFRIENNN",
-				"GURLFRIENNN"
-			],
-			implyingresp = [
-				"wanker.",
-				"you're so cool!",
-				">hurr"
-			];
+		var gfresp, implyingresp, lelresp,
+			args = match[1].split(" ");
 		switch (args[0]) {
-			case "gf":
+			case ">lel":
+			case "lel":
+				lelresp = [
+					">lel",
+					">mitch_",
+					"bitchslaps mitch_",
+					"mitchslaps mitch_",
+					"inflicts mitch_ upon himself.",
+					"lels mitch_'s backside."
+				];
 				setTimeout(function () {
-					gfresp = gfresp[Math.floor(Math.random()*gfresp.length)];
-					irc.say(input.context, gfresp);
+					irc.action(input.context, randReply(lelresp));
+				}, Math.floor(Math.random()*5000));
+				break;
+			case "gf":
+				gfresp = [
+					"<mitch_> GURLFRIENNN",
+					"GURLFRIENNN"
+				];
+				setTimeout(function () {
+					irc.say(input.context, randReply(gfresp));
 				}, Math.floor(Math.random()*5000));
 				break;
 			case ">implying":
+				implyingresp = [
+					"wanker.",
+					"you're so cool!",
+					">hurr"
+				];
 				setTimeout(function () {
-					implyingresp = implyingresp[Math.floor(Math.random()*implyingresp.length)];
-					irc.reply(input, implyingresp);
+					irc.reply(input, randReply(implyingresp));
 				}, Math.floor(Math.random()*5000));
 				break;
 			default:
