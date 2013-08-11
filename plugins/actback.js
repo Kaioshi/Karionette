1bafdb89f6552ad01b85d467cd9e1c451ea647e7
Kaioshi <waderr at gmail dot com>
2013-08-11 20:11:38 +0800

    [actback] add super basic question answering (yes/no/hurr)

--- plugins/actback.js
+++ plugins/actback.js
@@ -111,3 +111,103 @@ listen({
 		}, lib.randNum(3000,10000));
 	}
 });
+
+function questionReply() {
+	if (lib.chance(50)) { // yes and maybe
+		if (lib.chance(50)) { // yes
+			return lib.randSelect([
+				"yep", "yep.", "yep!",
+				"yes", "yes.", "yes!",
+				"yeah", "yeah.", "yeah!",
+				"yea", "yea.", "yea!",
+				"of course", "of course.", "of course!",
+				"without fail",
+				"beyond a doubt", "beyond a shadow of a doubt",
+				"by all means",
+				"definitely", "definitely.", "definitely!",
+				"affirmative",
+				"undoubtedly",
+				"naturally",
+				"I believe the answer is yes.",
+				"my magic 8-ball and I both agree that the answer is almost certainly without a doubt most likely yes.",
+				"yerp", "yERP", "YERP",
+				"absolutely", "absolutely.", "absolutely!",
+				"unquestionably yes",
+				"YESSSS",
+				"ahuh", "ahuh.", "ahuh!",
+				"mhm",
+				"mmm oh yeah"
+			]);
+		} else { // maybe
+			return lib.randSelect([
+				"maybe", "maybe?", "maybe!..", ".. maybe?",
+				"mebbe", "mebbe!", "mebbe?",
+				"possibly", "possibly!", "possibly?",
+				"perhaps", "perhaps.", "perhaps!",
+				"conceivably", "conceivably.",
+				"uncertainly", "uncertainly.", "uncertainly!",
+				"the odds are heavily in favour of maybe",
+				"it is within the realm of possibility",
+				"god willing", "jebus permitting", "JAYSUS PERMITTING",
+				"if it were at all possible, perhaps perchance",
+				"my magic 8-ball and I both agree that the answer is probably maybe. maybe.",
+				"may be.",
+				"maybe. maybe? may bee! A BEE OH GOD, RUN! RUN FOR YOUR LIVES"
+			]);
+		}
+	} else { // no and hurr
+		if (lib.chance(70)) { // no
+			return lib.randSelect([
+				"no", "no.", "no!",
+				"absolutely not",
+				"ABSOLUTELY NOT!",
+				"absolutely not.",
+				"nope", "nope.", "nope!",
+				"nah", "nah.", "nah!",
+				"nuh", "nuh.", "nuh!",
+				"all signs point to NUH.",
+				"NOPENOPENOPENOPENOPE",
+				"no. No. NO. NONONONONONONO",
+				"Nope. NOPE. NOPENOPENOPENOPE",
+				"NOOOOOOO",
+				"NO", "NO.", "NO!",
+				"Heck no.", "Heck NO", "HECK NO!",
+				"not this time",
+				"perhaps another time.",
+				"I'm in a season of "+lib.randSelect(["no", "NO", "NO!", "NOPE", "NOPE!", "NUH UH"]),
+				"Thanks, but no thanks.",
+				"Not possible",
+				"unpossible.",
+				"in another life.",
+				"I cry, but decline.",
+				"N to the O.",
+				"if only"
+			]);
+		} else { // hurr
+			return lib.randSelect([
+				"hurrr.", "-.-", "balls", "o_o", ".________.",
+				"hi!", "hello.", "an butt?",
+				"if you would you could you should you into mitch_?",
+				"I like your shoes",
+				"you got a purdy mouth",
+				"what do you think?",
+				"I put the question to you!",
+				"c'mon now.", "c'mon man.", "O_O",
+				";~;", "o_O", "O_o", "...", ". . .", "wtf", "D:", ":D", ":>", ">:(",
+				"here, take this and apply generously to your <bleep> twice a day until that clears up.. or it falls off."
+			]);
+		}
+	}
+}
+
+listen({
+	plugin: "actback",
+	handle: "actbackquestion",
+	regex: regexFactory.startsWith(["do you", "would you", "should I", "will you", "will I"]),
+	callback: function (input, match) {
+		setTimeout(function () {
+			irc.reply(input, questionReply());
+		}, lib.randNum(1000, 5000));
+	}
+});
+
