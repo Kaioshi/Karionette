cf597e402770b1f2907df187bbeba86ea063d8d7
Deide <paufawx@gmail.com>
2013-08-15 14:09:06 +0100

    Changed actback's actbackquestion regex to include all questions

--- plugins/actback.js
+++ plugins/actback.js
@@ -207,7 +207,7 @@ function questionReply() {
 listen({
 	plugin: "actback",
 	handle: "actbackquestion",
-	regex: regexFactory.startsWith(["does", "do", "would", "should", "will", "can", "are", "what", "is"]),
+	regex: /^Mari[,:] (\w+).+ ?$/i,
 	callback: function (input, match) {
 		setTimeout(function () {
 			irc.reply(input, questionReply());
