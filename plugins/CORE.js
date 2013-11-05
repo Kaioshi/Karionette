efdffa4237158055628d6a0d60477a4c9e96e7c7
Kaioshi <waderr at gmail dot com>
2013-11-05 23:56:28 +0800

    [CORE] move ;age to CORE, delete age.js

--- plugins/CORE.js
+++ plugins/CORE.js
@@ -242,3 +242,12 @@ cmdListen({
 	}
 });
 
+// Get Mari's age
+cmdListen({
+	command: "age",
+	help: "Tells you how old Mari is!",
+	callback: function (input) {
+		irc.say(input.context, "I am " 
+			+ lib.duration(new Date("1 May 2013 18:40:00 GMT"))	+ " old, but always sweet as sugar.");
+	}
+});
