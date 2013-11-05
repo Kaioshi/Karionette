b0bd37348754a8ba1b937dca61bfd9786f421136
Kaioshi <waderr at gmail dot com>
2013-11-06 02:06:55 +0800

    [btc] convert to caveman command listener

--- plugins/btc.js
+++ plugins/btc.js
@@ -1,23 +1,17 @@
 ﻿// gets BTC
-listen({
-	plugin: "btc",
-	handle: "btc",
-	regex: regexFactory.startsWith("btc"),
-	command: {
-		root: "btc",
-		options: "{Currency}",
-		help: "Retrieves high and low of bitcoin value. Defaults to USD if no argument is given"
-	},
-	callback: function (input, match) {
-		var uri, args = match[1].split(" ");
-		if (args[0]) {
-			uri = 'https://data.mtgox.com/api/2/BTC' + args[0] + '/money/ticker';
+cmdListen({
+	command: "btc",
+	help: "Retrieves high and low of bitcoin value. Defaults to USD if no argument is given",
+	callback: function (input) {
+		var uri;
+		if (input.args && input.args[0]) {
+			uri = 'https://data.mtgox.com/api/2/BTC' + input.args[0] + '/money/ticker';
 		} else {
 			uri = 'https://data.mtgox.com/api/2/BTCUSD/money/ticker';
 		}
 		web.get(uri, function (error, response, body) {
 			var result = JSON.parse(body);
-			irc.say(input.context, '1BTC ---> [HIGH] ' + result.data.high.display_short + ', [LOW] ' + result.data.low.display_short, false);
+			irc.say(input.context, '1 Bitcoin has recently been worth: [HIGH] ' + result.data.high.display_short + ', [LOW] ' + result.data.low.display_short, false);
 		});
 	}
 });
