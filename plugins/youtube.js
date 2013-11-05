b77f5d13aaeec806b281dc90cc5dabef04a1547b
Kaioshi <waderr at gmail dot com>
2013-11-06 04:39:52 +0800

    [youtube] convert to caveman command listener

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -1,25 +1,19 @@
 // youtubes!
-listen({
-	plugin: "youtube",
-	handle: "youtube",
-	regex: regexFactory.startsWith(["yt", "youtube"]),
-	command: {
-		root: "youtube",
-		help: "Searches youtube!",
-		syntax: "[Help] Syntax: " + config.command_prefix + "youtube <search terms>"
-	},
-	callback: function (input, match) {
-		var uri = "https://gdata.youtube.com/feeds/api/videos?q=" + match[1] + "&max-results=1&v=2&alt=json";
+function zero(n) {
+	return (n > 9 ? n : "0" + n);
+}
 
-		function zero(n) {
-			return (n > 9 ? n : "0" + n);
-		}
-
+cmdListen({
+	command: "yt",
+	help: "Searches youtube!",
+	syntax: config.command_prefix + "yt <search terms> - Example: "+config.command_prefix+
+		"yt we like big booty mitches",
+	callback: function (input) {
 		function ytCB(error, response, body) {
 			var link, title, rating, views, date;
 			body = JSON.parse(body).feed;
 			if (body.openSearch$totalResults.$t === 0 || !body.entry) {
-				irc.say(input.context, match[1] + " is not a thing on youtube. :<");
+				irc.say(input.context, input.data + " is not a thing on youtube. :<");
 				return;
 			}
 			link = "https://youtu.be/" + body.entry[0].link[0].href.split("&")[0].split("=")[1];
@@ -34,8 +28,12 @@ listen({
 			views = body.entry[0].yt$statistics.viewCount;
 			irc.say(input.context, title + rating + date + ", " + views + " views ~ " + link, false);
 		}
-
-		web.get(uri, ytCB);
+		
+		if (!input.args || !input.args[0]) {
+			irc.say(input.context, cmdHelp("yt", "syntax"));
+			return;
+		}
+		web.get("https://gdata.youtube.com/feeds/api/videos?q="+input.data+"&max-results=1&v=2&alt=json", ytCB);
 	}
 });
 
