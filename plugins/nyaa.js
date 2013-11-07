8dc92865321b1f7b2ee0c6519d93f199db521a4e
Kaioshi <waderr at gmail dot com>
2013-11-07 17:23:26 +0800

    [nyaa] if the regex fails a third time, give up and do a google search instead.

--- plugins/nyaa.js
+++ plugins/nyaa.js
@@ -32,21 +32,22 @@ var fs = require("fs"),
 
 cmdListen({
 	command: "nyaa",
-	help: "Tracks shows / groups on Nyaa",
+	help: "Nyaa.. it's a nyaa searcher!",
 	syntax: config.command_prefix+"nyaa [-u] <search term> - the -u flag removes any filtering. "+
 		"By default the filter is set to showing Trusted-quality Anime which has been subbed.",
 	callback: function (input) {
-		var entries, uri;
+		var entries;
 		if (!input.args) {
 			irc.say(input.context, cmdHelp("nyaa", "syntax"));
 			return;
 		}
 		if (input.args[0] === "-u") {
-			uri = "http://www.nyaa.se/?page=rss&cats=0_0&term="+input.args.slice(1).join(" ");
+			input.term = input.args.slice(1).join(" ");
+			input.uri = "http://www.nyaa.se/?page=rss&cats=0_0&term="+input.term;
 		} else {
-			uri = "http://www.nyaa.se/?page=rss&cats=1_37&filter=2&term="+input.data;
+			input.uri = "http://www.nyaa.se/?page=rss&cats=1_37&filter=2&term="+input.data;
 		}
-		web.get(uri, function (error, resp, body) {
+		web.get(input.uri, function (error, resp, body) {
 			if (body) {
 				body = ent.decode(body);
 				entries = [];
@@ -90,6 +91,18 @@ evListen({
 				if (reg) {
 					if (!rel[reg[1]]) rel[reg[1]] = {};
 					rel[reg[1]][reg[2]] = [];
+				} else { // fake a google search!
+					tmp = (input.channel ? lib.randSelect(ial.Active(input.channel)) : "yourself.");
+					if (tmp === input.nick) tmp = lib.randSelect(config.local_whippingboys)+".";
+					logger.warn("Failed to parse Nyaa results for this - doing a google search for now.");
+					irc.say(input.context, "Had trouble with Nyaa - here's the google result. Blame "+tmp);
+					input.command = "g";
+					input.data = "site:nyaa.se "+(input.term ? input.term : input.data);
+					input.raw = ":"+input.nick+"!"+input.address+" PRIVMSG "+input.context+" :g "+input.data;
+					input.args = [ "site:nyaa.se", input.data ];
+					lib.events.emit("Command: g", input);
+					globals.lastResults = results;
+					return;
 				}
 			}
 		}
@@ -102,7 +115,7 @@ evListen({
 					tmp = tmp+show+" :: ";
 				}
 			});
-			irc.say(input.context, ent.decode(tmp.slice(0,-4)));
+			irc.say(input.context, tmp.slice(0,-4));
 		});
 		tmp = null; reg = null; results = null;
 	}
