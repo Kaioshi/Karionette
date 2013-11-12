a5d6b204033c4c2d59cf6581ccff4b7ea55213e1
Kaioshi <waderr at gmail dot com>
2013-11-13 01:46:59 +0800

    [git] stop trying to be clever with the output

--- plugins/git.js
+++ plugins/git.js
@@ -8,11 +8,8 @@ cmdListen({
 		if (input.args && input.args[0] === "pull") { 
 			if (userLogin.isAdmin(input.user)) {
 				sys.exec("git pull", function (error, stdout, stderr) {
-					if (stdout === "Already up-to-date.\n") irc.say(input.context, stdout.slice(0,-1));
-					else {
-						reg = /\n ([0-9]+) files changed, ([0-9]+) insertions\(\+\), ([0-9]+) deletions\(\-\)\n/.exec(stdout);
-						irc.say(input.context, reg[1]+" file(s) changed; "+reg[2]+" insertions, "+reg[3]+" deletions.");
-					}
+					stdout = stdout.split("\n");
+					irc.say(input.context, stdout[stdout.length-2]);
 				});
 			} else {
 				irc.say(input.context, "Only admins may pull the git.");
