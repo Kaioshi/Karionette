bb974cd09a0602cdb70a15eda278f77313aa8dd9
Kaioshi <waderr at gmail dot com>
2013-11-18 21:30:22 +0800

    [airtime] adjust how we show last aired and increment the ep count for them

--- plugins/airtime.js
+++ plugins/airtime.js
@@ -1,10 +1,11 @@
 // Commie airtimes
+"use strict";
 cmdListen({
 	command: "airtime",
 	help: "Hopefully shows airtimes of Commie things.",
 	syntax: config.command_prefix+"airtime <show name>",
 	callback: function(input) {
-		web.get("http://c.milkteafuzz.com/api/1/search.json?q="+input.data, function (error, resp, body) {
+		web.get("http://c.milkteafuzz.com/api/1/search.json?q="+input.data.trim(), function (error, resp, body) {
 			body = JSON.parse(body);
 			if (body[0] && body[0]._id["$oid"]) {
 				web.get("https://c.milkteafuzz.com/api/1/shows/"+body[0]._id["$oid"]+".json", function (error, resp, body) {
@@ -23,7 +24,11 @@ cmdListen({
 						date = new Date(body.airtime["$date"]).valueOf();
 						now = new Date().valueOf();
 						if (date > now) airtime = "- The next episode airs in "+lib.duration(now, date)+".";
-						else airtime = "- Last aired: "+new Date(body.airtime["$date"]);
+						else {
+							// for some reason they don't increment the episodes as soon as the next one has aired.
+							if (body.episodes && body.episodes.current) body.episodes.current++;
+							airtime = "- Last aired "+lib.duration(date, now)+" ago.";
+						}
 					}
 					if (body.episodes) eps = body.episodes.current+" episodes ("+body.episodes.total+" total) ";
 					if (body.titles) {
@@ -38,3 +43,4 @@ cmdListen({
 		});
 	}
 });
+
