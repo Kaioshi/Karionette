7f48f0ab4fcd2f08bb42be9fbb16853e6f7036fb
Kaioshi <waderr at gmail dot com>
2014-04-08 15:58:52 +0800

    [weather] abolish -F and just show both at once

--- plugins/weather.js
+++ plugins/weather.js
@@ -3,16 +3,12 @@
 cmdListen({
 	command: "weather",
 	help: "Weather thing! Weathers.",
-	syntax: config.command_prefix+"weather [-F] <city / state & country>",
+	syntax: config.command_prefix+"weather <city / state & country>",
 	callback: function (input) {
-		var uri, temp, time, sunriseTime, sunsetTime, f, place;
+		var uri, temp, time, sunriseTime, sunsetTime, C, F, place;
 		if (!input.args) {
 			irc.say(input.context, cmdHelp("weather", "syntax"));
 			return;
-		}
-		if (input.args[0].toUpperCase() === "-F") {
-			input.data = input.args.slice(1).join(" ");
-			f = true;
 		}
 		uri = "http://api.openweathermap.org/data/2.5/weather?q="+input.data.trim();
 		web.get(uri, function (error, response, body) {
@@ -21,8 +17,11 @@ cmdListen({
 				irc.say(input.context, "Nope. API said: "+body.message);
 				return;
 			}
-			temp = (f ? (body.main.temp*(1.8)-459.67).toString() : (body.main.temp-273.15).toString());
-			temp = temp.slice(0, temp.indexOf(".")+3) + (f ? "F" : "C");
+			C = (body.main.temp-273.15).toString();
+			C = C.slice(0, C.indexOf(".")+3)+"C";
+			F = (body.main.temp*(1.8)-459.67).toString();
+			F = F.slice(0, F.indexOf(".")+3)+"F";
+			temp = C+" / "+F;
 			place = (body.name ? body.name+", " : "")+body.sys.country;
 			irc.say(input.context, "The temperature in "+place+" ("+body.coord.lon+", "+body.coord.lat+") is "+temp+".");
 		});
