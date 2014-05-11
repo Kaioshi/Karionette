6f2f11ba306be4d25575a6e48945a72353dbcb54
Kaioshi <waderr at gmail dot com>
2014-05-11 13:26:58 +0800

    [git] show all of the changes on pull

--- plugins/git.js
+++ plugins/git.js
@@ -1,15 +1,27 @@
-var sys = require('sys');
+var run = require('child_process').exec;
 
 cmdListen({
 	command: "git",
 	help: "git pull for people too lazy to open a shell",
 	callback: function (input) {
-		var reg;
+		var changes, i, l;
 		if (input.args && input.args[0] === "pull") { 
 			if (userLogin.isAdmin(input.user)) {
-				sys.exec("git pull", function (error, stdout, stderr) {
+				run("git pull", function (error, stdout, stderr) {
 					stdout = stdout.split("\n");
-					irc.say(input.context, stdout[stdout.length-2].trim());
+					changes = []; i = 0; l = stdout.length;
+					for (; i < l; i++) {
+						if (stdout[i][0] === " " && stdout[i][1] !== " ") {
+							changes.push([
+								"say",
+								input.context,
+								stdout[i].trim(),
+								false
+							]);
+						}
+					}
+					if (changes.length > 0) irc.rated(changes);
+					else irc.say(input.context, "Nothing changed?");
 				});
 			} else {
 				irc.say(input.context, "Only admins may pull the git.");
