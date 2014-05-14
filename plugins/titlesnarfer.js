1d1f55f9ee7dedb04cf168ac84926145f341493d
Kaioshi <waderr at gmail dot com>
2014-05-14 13:03:24 +0800

    [titlesnarfer] restore ;lasturl and ;urlstats

--- plugins/titlesnarfer.js
+++ plugins/titlesnarfer.js
@@ -40,10 +40,70 @@ function dura(secs) {
 	return ret.join(":");
 }
 
-function recordURL(nick, channel, url) {
+function lastUrl(channel, nick, match) {
+	var i, urls, mostRecent, index, entry, lnick,
+		fn = "data/urls/"+channel.toLowerCase()+".txt";
+	if (!fs.existsSync(fn)) return "I haven't seen any URLs here.";
+	urls = fs.readFileSync(fn).toString().split("\n");
+	i = urls.length;
+	if (match) match = match.toLowerCase();
+	if (nick) {
+		lnick = nick.toLowerCase();
+		while (i > 1) { // start from the bottom up since they're more recent.
+			i--;
+			if (urls[i].toLowerCase().indexOf(" "+lnick+" ") > -1) {
+				entry = urls[i].split(" ");
+				if (match && entry[0].toLowerCase().indexOf(match) === -1) {
+					continue;
+				}
+				if (!mostRecent || entry[2] > mostRecent) {
+					index = i;
+					mostRecent = entry[2];
+				}
+			}
+		}
+	} else {
+		index = i-1; // lowest entry, probably.
+		while (!urls[index]) index--; // mmm tailing newlines
+	}
+	if (index !== undefined) {
+		mostRecent = urls[index].split(" ");
+		urls = null; entry = null;
+		return mostRecent[1]+" linked "+mostRecent[0]+" "
+			+(mostRecent[3] ? "("+mostRecent.slice(3).join(" ")+") " : "")
+			+lib.duration(mostRecent[2], null, true)+" ago.";
+	}
+	return "Can't see any.";
+}
+
+function urlStats(channel, nick, match) {
+	var i, urls, count, lnick, entry,
+		fn = "data/urls/"+channel.toLowerCase()+".txt";
+	if (!fs.existsSync(fn)) return "I haven't seen any URLs here.";
+	urls = fs.readFileSync(fn).toString().split("\n");
+	i = urls.length; count = 0;
+	if (nick) {
+		lnick = nick.toLowerCase();
+		while (i > 1) {
+			i--;
+			if (urls[i].toLowerCase().indexOf(" "+lnick+" ") > -1) {
+				entry = urls[i].split(" ");
+				if (match && entry[0].toLowerCase().indexOf(match) === -1) {
+					continue;
+				}
+				count++;
+			}
+		}
+		return (match ? "I've seen "+lib.commaNum(count)+" URLs containing \""+match+"\" from "+nick+" here." :
+			"I've seen "+lib.commaNum(count)+" URLs from "+nick+" here.");
+	}
+	return "I've seen "+lib.commaNum(i)+" URLs here.";
+}
+
+function recordURL(nick, channel, url, title) {
 	var fn = "data/urls/"+channel.toLowerCase()+".txt";
 	if (!fs.existsSync(fn)) fs.writeFileSync(fn, "");
-	fs.appendFileSync(fn, url+" "+nick+" "+new Date().valueOf()+"\n");
+	fs.appendFileSync(fn, url+" "+nick+" "+new Date().valueOf()+(title ? " "+title+"\n" : "\n"));
 }
 
 function getURL(channel, url) {
@@ -64,29 +124,42 @@ function getURL(channel, url) {
 	urls = null;
 }
 
-function youtubeIt(context, id, host, old) {
+function youtubeIt(context, id, host, old, record) {
 	var uri = "https://gdata.youtube.com/feeds/api/videos/"+id+"?v=2&alt=json";
 	web.get(uri, function (error, response, body) {
-		var date, duration,
+		var date, duration, title,
 			views = "";
 		if (error) {
 			logger.error("[titlesnarfer[youtubeIt("+id+")]] error: "+error);
 			irc.say(context, "Something has gone awry.");
 			return;
 		}
-		body = JSON.parse(body).entry;
-		if (body["yt$statistics"] && body["yt$statistics"].viewCount) {
-			views = " - "+lib.commaNum(body["yt$statistics"].viewCount)+" views";
+		try {
+			body = JSON.parse(body).entry;
+			if (body["yt$statistics"] && body["yt$statistics"].viewCount) {
+				views = " - "+lib.commaNum(body["yt$statistics"].viewCount)+" views";
+			}
+			duration = dura(parseInt(body.media$group.yt$duration.seconds, 10));
+			date = new Date(body["media$group"]["yt$uploaded"]["$t"]);
+			date = zero(date.getDate())+"/"+zero(date.getMonth()+1)+"/"+date.getYear().toString().slice(1);
+			title = body.title["$t"]+" - ["+duration+"] "+date+views;
+			irc.say(context, title+(old ? " ("+old+")" : ""), false);
+		} catch (err) { // failed to JSON.parse
+			if (body.indexOf("Private video") > -1) {
+				title = "Private video.";
+				irc.say(context, title);
+			} else {
+				logger.warn("Failed to JSON.parse youtube result and it wasn't private.");
+			}
 		}
-		duration = dura(parseInt(body.media$group.yt$duration.seconds, 10));
-		date = new Date(body["media$group"]["yt$uploaded"]["$t"]);
-		date = zero(date.getDate())+"/"+zero(date.getMonth()+1)+"/"+date.getYear().toString().slice(1);
-		irc.say(context, body.title["$t"]+" - ["+duration+"] "+date+views+(old ? " ("+old+")" : ""), false);
+		if (record) {
+			recordURL(record[0], record[1], record[2], title);
+		}
 		date = null; views = null; body = null; response = null; error = null;
 	});
 }
 
-function sayTitle(context, uri, length, imgur, old) {
+function sayTitle(context, uri, length, imgur, old, record) {
 	var reg, title;
 	web.get(uri.href, function (error, response, body) {
 		if (error) {
@@ -99,7 +172,7 @@ function sayTitle(context, uri, length, imgur, old) {
 		}
 		reg = /<title?[^>]+>([^<]+)<\/title>/i.exec(body.replace(/\n|\t|\r/g, ""));
 		if (!reg || !reg[1]) return;
-		title = reg[1].trim();
+		title = reg[1];
 		if (title.toLowerCase().indexOf(uri.host) > -1) {
 			reg = new RegExp(" "+uri.host+" ?", "ig");
 			title = title.replace(reg, "");
@@ -108,8 +181,11 @@ function sayTitle(context, uri, length, imgur, old) {
 		if (imgur) { // I know there are a lot of imgur corner cases, but it's really common.
 			if (title === "imgur: the simple image sharer") return; // deal with it
 		}
-		irc.say(context, lib.singleSpace(lib.decode(title))+
-			" ~ "+uri.host.replace("www.", "")+(old ? " ("+old+")" : ""), false);
+		title = lib.singleSpace(lib.decode(title));
+		irc.say(context, title+" ~ "+uri.host.replace("www.", "")+(old ? " ("+old+")" : ""), false);
+		if (record) {
+			recordURL(record[0], record[1], record[2], title);
+		}
 		reg = null; title = null; body = null; response = null; error = null;
 	}, length);
 }
@@ -119,28 +195,28 @@ evListen({
 	event: "PRIVMSG",
 	regex: /^:[^ ]+ PRIVMSG #[^ ]+ :.*((?:https?:\/\/)[^\x01 ]+)/i,
 	callback: function (input) {
-		var uri, ext, allow, old, length = 10000;
+		var uri, ext, allow, old, record, length = 10000;
 		
 		if (input.args) return; // don't process urls in commands
-		old = getURL(input.channel, input.match[1]);
-		if (!old) recordURL(input.nick, input.channel, input.match[1]);
+		old = getURL(input.channel, input.match[1]) || false;
+		if (!old) record = [ input.nick, input.channel, input.match[1] ];
 		uri = url.parse(input.match[1]);
 		if (uri.host.indexOf("youtube.com") > -1 && uri.path.indexOf("v=") > -1) {
-			youtubeIt(input.context, /v=([^ &\?]+)/i.exec(uri.path)[1], uri.host, old);
+			youtubeIt(input.context, /v=([^ &\?]+)/i.exec(uri.path)[1], uri.host, old, record);
 			return;
 		}
 		if (uri.host.indexOf("youtu.be") > -1 && uri.path.length > 1) {
-			youtubeIt(input.context, /^\/([^ &\?]+)/.exec(uri.path)[1], uri.host, old);
+			youtubeIt(input.context, /^\/([^ &\?]+)/.exec(uri.path)[1], uri.host, old, record);
 			return;
 		}
 		if (uri.host === "imgur.com") {
-			sayTitle(input.context, uri, length, true, old);
+			sayTitle(input.context, uri, length, true, old, record);
 			return;
 		}
 		if (uri.host === "i.imgur.com" && uri.href.slice(-4).match(/\.jpg|\.png|\.gif/i)) {
 			uri.path = uri.path.slice(0,-4);
 			uri.href = uri.href.slice(0,-4);
-			sayTitle(input.context, uri, length, true, old);
+			sayTitle(input.context, uri, length, true, old, record);
 			return;
 		}
 		ext = /.*\.([a-zA-Z0-9]+)$/.exec(uri.path);
@@ -152,7 +228,40 @@ evListen({
 		}
 		// ton of garbage in the first 15000 characters. o_O
 		if (uri.host.indexOf("kotaku") > -1) length = 20000;
-		sayTitle(input.context, uri, length, false, old);
+		sayTitle(input.context, uri, length, false, old, record);
 	}
 });
 
+cmdListen({
+	command: "lasturl",
+	help: "Shows the last URLs people posted!",
+	syntax: config.command_prefix+"lasturl [<nick>] [<term>] - Example: "
+		+config.command_prefix+"lasturl ranma goatse",
+	callback: function (input) {
+		var searchTerm, target;
+		if (!input.channel) {
+			irc.say(input.context, "This can only be used in channels.");
+			return;
+		}
+		target = (input.args ? input.args[0] : null);
+		searchTerm = (input.args && input.args.length > 1 ? input.args.slice(1).join(" ") : null);
+		irc.say(input.context, lastUrl(input.channel, target, searchTerm), false);
+	}
+});
+
+cmdListen({
+	command: "urlstats",
+	help: "Shows URL stats!",
+	syntax: config.command_prefix+"urlstats [<nick>] [<term>] - Example: "
+		+config.command_prefix+"urlstats ranma imgur",
+	callback: function (input) {
+		var searchTerm, target;
+		if (!input.channel) {
+			irc.say(input.context, "This can only be used in channels.");
+			return;
+		}
+		target = (input.args ? input.args[0] : null);
+		searchTerm = (input.args && input.args.length > 1 ? input.args.slice(1).join(" ") : null);
+		irc.say(input.context, urlStats(input.channel, target, searchTerm), false);
+	}
+});
