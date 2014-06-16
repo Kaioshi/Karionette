b7e3692e6694fbda233c9331eb64ea6fbba0f431
Kaioshi <waderr at gmail dot com>
2014-06-16 14:32:09 +0800

    do custom urls in shorten

--- plugins/shorten.js
+++ plugins/shorten.js
@@ -1,5 +1,60 @@
 'use strict';
 
+function makeKey(len) {
+	var i = 0, n, ret = "";
+	len = len || 8;
+	for (; i < len; i++) {
+		n = Math.floor(Math.random()*9);
+		if (n >= 5) 
+			ret += lib.randSelect([ "A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f" ]);
+		else
+			ret += n;
+	}
+	return ret;
+}
+
+function makeUrl(url) {
+	var word, key, ext = url.slice(url.lastIndexOf("."));
+	switch (ext) {
+		case ".png":
+		case ".gif":
+		case ".jpeg":
+		case ".jpg":
+		case ".bmp":
+			word = "img_";
+			break;
+		case ".txt":
+			word = "txt_";
+			break;
+		case ".avi":
+		case ".mp4":
+		case ".mkv":
+		case ".wmv":
+		case ".rm":
+		case ".rmvb":
+		case ".mov":
+		case ".mpg":
+		case ".mpeg":
+		case ".m2v":
+		case ".webm":
+			word = "vid_";
+			break;
+		case ".mp3":
+		case ".ogg":
+		case ".wav":
+		case ".flac":
+		case ".m4a":
+			word = "snd_";
+			break;
+		default:
+			word = lib.randSelect([ "wat", "drp", "hrp", "lel", "wot", "dckbtt", "pnis",
+				"hey", "sup", "heh", "fsrs", "srs", "yep", "tchit" ]);
+			word = word+"_";
+			break;
+	}
+	return word+makeKey(9-word.length);
+}
+
 cmdListen({
 	command: "shorten",
 	help: "Shortens URLs!",
@@ -26,7 +81,8 @@ cmdListen({
 			url = input.data;
 			break;
 		}
-		web.get("http://"+gd+"/create.php?format=simple&url="+url.trim(), function (error, response, body) {
+		url = url.trim();
+		web.get("http://"+gd+"/create.php?format=simple&url="+url+"&shorturl="+makeUrl(url), function (error, response, body) {
 			irc.say(input.context, body);
 		});
 	}
