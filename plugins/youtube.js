37edd18e9b9268e124824260a3f6e07a360a948a
kaioshi <waderr@gmail.com>
2015-05-08 18:48:09 +0800

    [youtube] handle channel response properly

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -23,7 +23,7 @@ cmdListen({
 	syntax: config.command_prefix + "yt <search terms> - Example: "+config.command_prefix+
 		"yt we like big booty mitches",
 	callback: function (input) {
-		var uri;
+		var uri, resp, desc;
 		if (!input.args) {
 			irc.say(input.context, cmdHelp("yt", "syntax"));
 			return;
@@ -34,22 +34,36 @@ cmdListen({
 		}
 		uri = "https://www.googleapis.com/youtube/v3/search?part=id&maxResults=1&q="+input.data+"&safeSearch=none&key="+config.api.youtube;
 		web.get(uri, function (error, response, body) {
-			var resp = JSON.parse(body);
+			resp = JSON.parse(body);
 			if (!resp.items.length) {
 				irc.say(input.context, "\""+input.data+"\" is not a thing on YouTube.", false);
 				return;
 			}
-			web.youtube(resp.items[0].id.videoId, function (yt) {
-				if (yt.error) {
-					if (yt.error.reason === "keyInvalid")
-						irc.say(input.context, "Your YouTube API key is invalid. Get another: https://developers.google.com/youtube/v3/getting-started");
-					else
-						irc.say(input.context, yt.error.message+": "+yt.error.reason);
-					return;
-				}
-				irc.say(input.context, yt.title+" - ["+yt.duration+"] "+yt.date.split("T")[0]+
-					" - "+yt.channel+" - "+lib.commaNum(yt.views)+" views ~ "+yt.link, false);
-			});
+			if (resp.items[0].id.kind === "youtube#video") {
+				web.youtube(resp.items[0].id.videoId, function (yt) {
+					if (yt.error) {
+						if (yt.error.reason === "keyInvalid")
+							irc.say(input.context, "Your YouTube API key is invalid. Get another: https://developers.google.com/youtube/v3/getting-started");
+						else
+							irc.say(input.context, yt.error.message+": "+yt.error.reason);
+						return;
+					}
+					irc.say(input.context, yt.title+" - ["+yt.duration+"] "+yt.date.split("T")[0]+
+						" - "+yt.channel+" - "+lib.commaNum(yt.views)+" views ~ "+yt.link, false);
+				});
+			} else if (resp.items[0].id.kind === "youtube#channel") {
+				uri = "https://www.googleapis.com/youtube/v3/channels?part=snippet&id="+
+					resp.items[0].id.channelId+"&maxResults=1&fields=items&key="+config.api.youtube;
+				web.get(uri, function (error, response, body) {
+					resp = JSON.parse(body);
+					desc = (resp.items[0].snippet.description.length ? ": "+resp.items[0].snippet.description : "");
+					irc.say(input.context, resp.items[0].snippet.title+desc+
+						" - Channel launched on "+resp.items[0].snippet.publishedAt.split("T")[0]+
+						" ~ https://youtube.com/user/"+resp.items[0].snippet.title, false);
+				});
+			} else {
+				irc.say(input.context, "YouTube returned a "+resp.items[0].id.kind+" response which I don't handle.");
+			}
 		});
 	}
 });
