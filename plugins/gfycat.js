9a60ce406ba757ee1b0870cdffb243b04f2b42c5
Kaioshi <waderr@gmail.com>
2015-05-16 12:55:24 +0800

    [gfycat] switch to arglen

--- plugins/gfycat.js
+++ plugins/gfycat.js
@@ -5,17 +5,14 @@ cmdListen({
 	help: "Gfycat frontend.",
 	syntax: config.command_prefix+"gfy <url> - Example: "+
 		config.command_prefix+"gfy http://i.imgur.com/N2FEP.gif",
+	arglen: 1,
 	callback: function (input) {
 		var result, sizeReduction;
-		if (!input.args) {
-			irc.say(input.context, cmdHelp("gfycat", "syntax"));
-			return;
-		}
 		input.args[0] = input.args[0].trim();
 		web.get("http://upload.gfycat.com/transcode?fetchUrl="+input.args[0], function (error, response, body) {
 			result = JSON.parse(body);
 			if (result.error) {
-				irc.say(input.context, "Gfycat said \""+result.error+"\".");
+				irc.say(input.context, "Gfycat said \""+result.error+"\"");
 				return;
 			}
 			sizeReduction = Math.round((result.gifSize - result.gfysize)/1024);
