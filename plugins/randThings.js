40be6f1f595e605113e2dfd3cb18f76e40a8060c
Kaioshi <waderr@gmail.com>
2015-05-16 13:31:44 +0800

    [randThings] switch to arglen, cleanup

--- plugins/randThings.js
+++ plugins/randThings.js
@@ -4,12 +4,12 @@ cmdListen({
 	command: "random",
 	help: "Picks a random element from your comma separated list.",
 	syntax: config.command_prefix+"random list, of, things, to select, from",
+	arglen: 1,
 	callback: function (input) {
-		if (!input.args) {
+		if (input.data.indexOf(",") === -1)
 			irc.say(input.context, cmdHelp("random", "syntax"));
-			return;
-		}
-		irc.say(input.context, lib.randSelect(input.data.split(",")).trim());
+		else
+			irc.say(input.context, lib.randSelect(input.data.split(",")).trim());
 	}
 });
 
@@ -18,30 +18,26 @@ cmdListen({
 	help: "add to randThings",
 	syntax: config.command_prefix+"thing <add/remove> <thing> - Example: "+config.command_prefix+
 		"thing add ranma's dodgy undies",
+	arglen: 2,
 	callback: function (input) {
 		var entry;
-		
-		if (!input.args || input.args.length < 2) {
-			irc.say(input.context, cmdHelp("thing", "syntax"));
-			return;
-		}
 		switch (input.args[0].toLowerCase()) {
-			case 'add':
-				randDB.saveOne(input.args.slice(1).join(" "));
-				irc.say(input.context, 'Added :)');
-				break;
-			case 'remove':
-				entry = randDB.getOne(input.args.slice(1).join(" "));
-				if (entry) {
-					randDB.removeOne(entry);
-					irc.say(input.context, "Removed :S");
-				} else {
-					irc.say(input.context, "I know of no such thing.");
-				}
-				break;
-			default:
-				irc.say(input.context, cmdHelp("thing", "syntax"));
-				break;
+		case 'add':
+			randDB.saveOne(input.args.slice(1).join(" "));
+			irc.say(input.context, 'Added :)');
+			break;
+		case 'remove':
+			entry = randDB.getOne(input.args.slice(1).join(" "));
+			if (entry) {
+				randDB.removeOne(entry);
+				irc.say(input.context, "Removed :S");
+			} else {
+				irc.say(input.context, "I know of no such thing.");
+			}
+			break;
+		default:
+			irc.say(input.context, cmdHelp("thing", "syntax"));
+			break;
 		}
 	}
 });
