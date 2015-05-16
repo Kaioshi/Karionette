54a71dbc527be358f6936d846210b228bd2b89b3
Kaioshi <waderr@gmail.com>
2015-05-16 13:43:40 +0800

    [ud] switch to arglen, cleanup

--- plugins/ud.js
+++ plugins/ud.js
@@ -4,12 +4,9 @@ cmdListen({
 	help: "Look up something from Urban dictionary!",
 	syntax: config.command_prefix+"ud <term> - Example: "+config.command_prefix+
 		"ud scrobble",
+	arglen: 1,
 	callback: function (input) {
 		var result, i, tmp, def;
-		if (!input.args) {
-			irc.say(input.context, cmdHelp("ud", "syntax"));
-			return;
-		}
 		web.get("http://api.urbandictionary.com/v0/define?term="+input.data, function (error, response, body) {
 			result = JSON.parse(body);
 			if (result.result_type === "no_results" || result.list.length === 0) {
@@ -18,11 +15,11 @@ cmdListen({
 				tmp = "1) "+lib.singleSpace(result.list[0].definition.replace(/\n|\r|\t/g, " "));
 				if (result.list.length > 1) {
 					for (i = 1; i < result.list.length; i++) {
-						if (tmp.length >= 300) break;
+						if (tmp.length >= 300)
+							break;
 						def = lib.singleSpace(result.list[i].definition.replace(/\n|\r|\t/g, " "));
-						if (def.length <= (300-tmp.length)) {
+						if (def.length <= (300-tmp.length))
 							tmp += " "+(i+1)+") "+def;
-						}
 					}
 				}
 				irc.say(input.context, tmp, false);
