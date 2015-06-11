487a04a1a7544112d716b218d5538a723df66f64
Kaioshi <waderr@gmail.com>
2015-06-12 01:52:10 +0800

    [google] convert to promises

--- plugins/google.js
+++ plugins/google.js
@@ -7,17 +7,17 @@ cmdListen({
 	syntax: config.command_prefix+"g <search term> - Example: "+config.command_prefix+"g puppies",
 	arglen: 1,
 	callback: function (input) {
-		web.google(input.data.trim(), function (error, hits, results) {
-			if (hits > 0) {
-				if (config.google_format) {
-					results[0].b = '\x02';
-					irc.say(input.context, lib.formatOutput(config.google_format, results[0]), false, 1);
-				} else {
-					irc.say(input.context, lib.formatOutput("{title} ~ {url} ~ {content}", results[0]), false, 1);
-				}
+		web.google(input.data.trim()).then(function (results) {
+			if (config.google_format) {
+				results[0].b = '\x02';
+				irc.say(input.context, lib.formatOutput(config.google_format, results[0]), false, 1);
 			} else {
-				irc.say(input.context, "Couldn't find it. :<");
+				irc.say(input.context, lib.formatOutput("{title} ~ {url} ~ {content}", results[0]), false, 1);
 			}
+		}, function (error) {
+			irc.say(input.context, error.message, false);
+		}).catch(function (error) {
+			logger.error("Error in ;google -> ", error);
 		});
 	}
 });
@@ -38,22 +38,19 @@ cmdListen({ // this will stop working soon~
 	syntax: config.command_prefix+"gi puppies",
 	arglen: 1,
 	callback: function (input) {
-		var uri;
-		uri = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&safe=moderate&rsz=1&q="+input.data;
-		web.get(uri, function (error, response, body) {
-			if (error) {
-				irc.say(input.context, "Something has gone awry.");
-				logger.error("[google-images] error looking up " + input.data + " -> " + error);
-				return;
-			}
-			body = JSON.parse(body);
-			if (body && body.responseData && body.responseData.results && body.responseData.results[0]) {
-				body = body.responseData.results;
-				irc.say(input.context, lib.decode(body[0].titleNoFormatting)+
-					" ("+body[0].width+"x"+body[0].height+"): "+body[0].url);
+		web.fetch("http://ajax.googleapis.com/ajax/services/search/images?v=1.0&safe=moderate&rsz=1&q="+input.data)
+		.then(function (body) {
+			var data = JSON.parse(body);
+			if (data && data.responseData && data.responseData.results && data.responseData.results[0]) {
+				data = data.responseData.results[0];
+				irc.say(input.context, lib.decode(data.titleNoFormatting)+
+					" ("+data.width+"x"+data.height+"): "+data.url, false);
 			} else {
 				irc.say(input.context, "No image found. :<");
 			}
+		}, function (error) {
+			irc.say(input.context, "Something has gone awry.");
+			logger.error("[google-images] error looking up " + input.data + " -> " + error);
 		});
 	}
 });
