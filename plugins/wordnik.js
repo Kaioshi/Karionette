b184fe6f7ed652fdad03478df3681e4ed8ddcb60
Kaioshi <waderr@gmail.com>
2015-06-12 10:03:27 +0800

    [wordnik] switch to web.json

--- plugins/wordnik.js
+++ plugins/wordnik.js
@@ -5,7 +5,7 @@ cmdListen({
 	syntax: config.command_prefix+"define <word/phrase> - Example: "+config.command_prefix+"define butt",
 	arglen: 1,
 	callback: function (input) {
-		var uri, i, l, definitions, query;
+		var uri, i, definitions, query;
 		if (!config.api.wordnik) {
 			irc.say(input.context, "The wordnik plugin requires an API key to be present in config, go to "+
 				"http://developer.wordnik.com to get one.");
@@ -15,16 +15,9 @@ cmdListen({
 		uri = "http://api.wordnik.com:80/v4/word.json/"+query+
 			"/definitions?limit=3&includeRelated=true&sourceDictionaries=wordnet,wiktionary&useCanonical=false&includeTags=false&api_key="+
 			config.api.wordnik;
-		web.get(uri, function (error, response, body) {
-			try {
-				body = JSON.parse(body);
-			} catch (e) {
-				globals.lastFailedWordnik = body;
-				logger.warn("Wordnik returned non-JSON");
-				return;
-			}
-			if (body.length === 0) {
-				irc.say(input.context, "Couldn't find it. "+lib.randSelect([
+		web.json(uri).then(function (resp) {
+			if (resp.length === 0) {
+				throw Error("Couldn't find it. "+lib.randSelect([
 					"You only have yourself to blame.",
 					"I'm sorry.",
 					"Sorry ;<",
@@ -33,23 +26,17 @@ cmdListen({
 					";_;",
 					"Blame "+(input.context[0] === "#" ? lib.randSelect(ial.Active(input.context))+"." : "yourself.")
 				]));
-				return;
 			}
-			for (definitions = " -", i = 0, l = body.length; i < l; i++)
-				definitions += " "+(i+1)+") ["+body[i].partOfSpeech+"] "+body[i].text;
+			for (definitions = " -", i = 0; i < resp.length; i++)
+				definitions += " "+(i+1)+") ["+resp[i].partOfSpeech+"] "+resp[i].text;
 			uri = "http://api.wordnik.com:80/v4/word.json/"+query+"/topExample?useCanonical=false&api_key="+config.api.wordnik;
-			web.get(uri, function (error, response, body) {
-				try {
-					body = JSON.parse(body);
-				} catch (e) {
-					globals.lastFailedWordnik = body;
-					logger.warn("Wordnik returned non-JSON");
-					return;
-				}
-				irc.say(input.context, lib.singleSpace(query+definitions), false);
-				if (body.text && body.title)
-					irc.say(input.context, lib.decode(lib.singleSpace(body.text+" - "+body.title)), false);
-			});
+			return web.json(uri);
+		}).then(function (resp) {
+			irc.say(input.context, lib.singleSpace(query+definitions), false);
+			if (resp.text && resp.title)
+				irc.say(input.context, lib.decode(lib.singleSpace(resp.text+" - "+resp.title)), false);
+		}, function (error) {
+			irc.say(input.context, error.message);
 		});
 	}
 });
