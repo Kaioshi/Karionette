199b3309af1058da4c387d8882a63ffe56d59c56
David <deide@live.co.uk>
2015-12-19 19:12:31 +0000

    Fix google image search trimming

--- plugins/google.js
+++ plugins/google.js
@@ -40,8 +40,8 @@ bot.command({
 	arglen: 1,
 	callback: function googleImageSearch(input) {
 		web.googleImage(input.data.trim()).then(function (results) {
-            let image = results[0].substring(0, results[0].lastIndexOf("?"));
-			irc.say(input.context, lib.formatOutput("{title} ~ {url}", image), 1);
+            results[0].url = results[0].url.substring(0, results[0].url.lastIndexOf("?"));
+			irc.say(input.context, lib.formatOutput("{title} ~ {url}", results[0]), 1);
 		}, function (error) {
 			irc.say(input.context, error.message);
 		}).catch(function (error) {
