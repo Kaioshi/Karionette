af6707737633364ddf9c5540477b0b9c1c6a069a
David <deide@live.co.uk>
2015-12-19 14:31:53 +0000

    Migrate google image plugin to new custom search engine

--- plugins/google.js
+++ plugins/google.js
@@ -6,7 +6,7 @@ bot.command({
 	help: "Google search - returns the first hit.",
 	syntax: config.command_prefix+"g <search term> - Example: "+config.command_prefix+"g puppies",
 	arglen: 1,
-	callback: function (input) {
+	callback: function googleSearch(input) {
 		web.google(input.data.trim()).then(function (results) {
 			if (config.google_format) {
 				results[0].b = "\x02";
@@ -27,28 +27,23 @@ bot.command({ // for ranmabutts
 	help: "Constructs a google query",
 	syntax: config.command_prefix+"gr <search term> - Example: "+config.command_prefix+"gr puppies vs. kittens?",
 	arglen: 1,
-	callback: function (input) {
+	callback: function googleSearchForRanma(input) {
 		irc.say(input.context, "https://google.com/search?q="+encodeURIComponent(input.data.trim()));
 	}
 });
 
-bot.command({ // this will stop working soon~
+bot.command({
 	command: "gi",
 	help: "Google image search - returns the first hit.",
 	syntax: config.command_prefix+"gi puppies",
 	arglen: 1,
-	callback: function (input) {
-		var uri = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&safe=moderate&rsz=1&q="+input.data;
-		web.json(uri).then(function (data) {
-			if (data && data.responseData && data.responseData.results && data.responseData.results[0]) {
-				data = data.responseData.results[0];
-				irc.say(input.context, lib.decode(data.titleNoFormatting)+" ("+data.width+"x"+data.height+"): "+data.url);
-			} else {
-				irc.say(input.context, "No image found. :<");
-			}
+	callback: function googleImageSearch(input) {
+		web.googleImage(input.data.trim()).then(function (results) {
+			irc.say(input.context, lib.formatOutput("{title} ~ {url}", results[0]), 1);
 		}, function (error) {
-			irc.say(input.context, "Something has gone awry.");
-			logger.error("[google-images] error looking up " + input.data + " -> " + error);
+			irc.say(input.context, error.message);
+		}).catch(function (error) {
+			logger.error("Error in ;google -> ", error);
 		});
 	}
 });
