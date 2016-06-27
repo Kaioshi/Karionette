1eec33767953bd26368c29d0b6c7743eff1b044f
Kaioshi <waderr@gmail.com>
2016-06-27 17:35:25 +0800

    [boot] update

--- boot.js
+++ boot.js
@@ -1,43 +1,28 @@
-// this file is so goddamn messy.
 "use strict";
 
-global.globals = {
-	os: process.platform,
-	startTime: new Date()
-};
+let plugin, replPrompt = "", useGc = true, gcInterval = 10000, mwInterval = 30000, repl = true;
+
+global.globals = { startTime: Date.now() };
 
 if (!require("fs").existsSync("config")) {
 	console.error(" * NO config FOUND, SEE config.example");
 	process.exit(1);
 }
 
-let config = require("./lib/config.js")(),
-	edgar = require("./lib/edgar.js")(),
-	logger = require("./lib/logger.js")(config, edgar),
-	ial = require("./lib/ial.js")(config),
-	lib = require("./lib/funcs.js")(logger),
-	ticker = require("./lib/ticker.js")(lib, edgar),
-	Plugin = require("./lib/plugin.js")(logger, config),
-	fragDB = require("./lib/fragDB.js")(lib, logger),
-	DB = require("./lib/db.js")(lib, logger),
-	logins = require("./lib/login.js")(config, ial, DB, edgar.event),
-	ignore = require("./lib/ignore.js")(DB, lib, ial),
-	web = require("./lib/web.js")(lib, logger, config),
-	words = require("./lib/words.js")(lib, config, logger, web, DB),
-	alias = require("./lib/alias.js")(DB, lib, config, ial, words),
-	perms = require("./lib/perms.js")(DB, logger, ial, logins),
-	bot = require("./lib/bot.js")(lib, config, logger, edgar, ial, perms, words, logins, alias, ignore),
-	replPrompt = "", gc = true, gcInterval = 10000, mwInterval = 30000, repl = true;
+process.on("uncaughtException", function caughtUncaughtException(err) {
+	console.error(err);
+});
 
-processArgs(process.argv.slice(2));
-
-global.irc = new require("./lib/irc.js")(config, bot, logger);
+plugin = require("./plugin.js")(global.globals);
+plugin.loadCorePlugins([ // the order matters
+	"bot", "ticker", "config", "logger", "lib", "web", "db", "words",
+	"ial", "alias", "ignore", "logins", "perms", "bot-parse", "irc", "admin",
+	"core-events", "logins-events", "ial-events", "perms-events", "alias-events", "config-events"
+]);
+plugin.loadOptionalPlugins();
 
 function processArgs(args) {
-	var slicelen,
-		memWatch = function () {
-			lib.memReport();
-		};
+	let slicelen;
 	while (args.length > 0) {
 		slicelen = 1;
 		switch (args[0]) {
@@ -61,7 +46,7 @@ function processArgs(args) {
 					slicelen = 2;
 				}
 			}
-			setInterval(memWatch, mwInterval);
+			setInterval(plugin.sandbox.lib.memReport, mwInterval);
 			break;
 		case "prompt":
 			if (args[1]) {
@@ -74,77 +59,40 @@ function processArgs(args) {
 				slicelen = 2;
 				gcInterval = parseInt(args[1], 10) * 1000;
 			} else {
-				logger.warn("gc-interval needs a number in seconds as it's argument. Using default.");
+				plugin.sandbox.logger.warn("gc-interval needs a number in seconds as it's argument. Using default.");
 			}
 			break;
 		case "nogc":
-			gc = false;
+			useGc = false;
 			break;
 		case "nocmd":
 			repl = false;
 			break;
 		default:
-			logger.warn("Invalid argument: "+args[0]);
+			plugin.sandbox.logger.warn("Invalid argument: "+args[0]);
 			break;
 		}
 		args = args.slice(slicelen);
 	}
 }
 
-if (gc) {
+processArgs(process.argv.slice(2));
+
+if (useGc) {
 	if (!global.gc) {
-		logger.warn("You need to run node with --expose-gc if you want reasonable garbage collection.");
-		logger.warn("Run with the \"nogc\" option to suppress this warning.");
+		plugin.sandbox.logger.warn("You need to run node with --expose-gc if you want reasonable garbage collection.");
+		plugin.sandbox.logger.warn("Run with the \"nogc\" option to suppress this warning.");
 	} else {
-		setInterval(function () {
-			global.gc();
-		}, gcInterval);
+		setInterval(global.gc, gcInterval);
 	}
 }
 
-Plugin.setupSandbox({ // console and setInterval not used by any plugins as of 2015-06-29
-	irc: global.irc,
-	config: config,
-	setTimeout: setTimeout,
-	emitEvent: edgar.emitEvent,
-	web: web,
-	DB: DB,
-	fragDB: fragDB,
-	lib: lib,
-	ial: ial,
-	ignore: ignore,
-	logins: logins,
-	ticker: ticker,
-	require: require,
-	bot: bot,
-	logger: logger,
-	words: words,
-	perms: perms,
-	globals: globals
-});
-
-irc.reload = function (plugin) {
-	if (!plugin) {
-		Plugin.loadAll("core", "plugins/core");
-		Plugin.loadAll("optional", "plugins");
-	} else {
-		Plugin.loadOne(plugin);
-	}
-};
-
-process.on("uncaughtException", function caughtUncaughtExcaption(err) {
-	logger.error("Uncaught Exception: ", err);
-});
-
-Plugin.loadAll("core", "plugins/core/");
-Plugin.loadAll("optional", "plugins/");
-
-irc.open({
-	server: config.server,
-	port: config.port,
-	nickname: config.nickname[0],
-	username: config.username,
-	realname: config.realname
+plugin.sandbox.irc.open({
+	server: plugin.sandbox.config.server,
+	port: plugin.sandbox.config.port,
+	nickname: plugin.sandbox.config.nickname[0],
+	username: plugin.sandbox.config.username,
+	realname: plugin.sandbox.config.realname
 });
 
 if (repl)
