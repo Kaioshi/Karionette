c015c1b05e49bd6fd22e8223bd6c61fd11c5e597
Kaioshi <waderr@gmail.com>
2016-06-27 17:34:13 +0800

    [core/alias-events] update

--- core/alias-events.js
+++ core/alias-events.js
@@ -1,15 +1,4 @@
 "use strict";
-var aliasDB = new DB.Json({filename: "alias/alias"}),
-	varDB = new DB.Json({filename: "alias/vars"}),
-	helpDB = new DB.Json({filename: "alias/help"});
-
-function aliasIsCommand(alias) {
-	var i, commands = bot.cmdList();
-	for (i = 0; i < commands.length; i++) {
-		if (commands[i] === alias)
-			return true;
-	}
-}
 
 // Handles Alias interface
 bot.command({
@@ -20,7 +9,7 @@ bot.command({
 		config.command_prefix+"alias add mal g site:myanimelist.net {args*}",
 	arglen: 1,
 	callback: function (input) {
-		var aliasList, alias, cmd, aliasString, help;
+		let aliasList, aliasEntry, cmd, aliasString, help;
 		if (input.args[1])
 			cmd = input.args[1].toLowerCase();
 		aliasString = input.args.slice(2).join(" ");
@@ -30,8 +19,8 @@ bot.command({
 				irc.say(input.context, "Which alias do you want to set help for?");
 				return;
 			}
-			alias = aliasDB.getOne(cmd);
-			if (!alias) {
+			aliasEntry = alias.db.getOne(cmd);
+			if (!aliasEntry) {
 				irc.say(input.context, "There is no such alias.");
 				return;
 			}
@@ -39,14 +28,14 @@ bot.command({
 				irc.say(input.context, "You need to own the \""+cmd+"\" alias to set it's help.");
 				return;
 			}
-			if (aliasIsCommand(cmd)) {
+			if (bot.cmdExists(cmd)) {
 				irc.say(input.context, "You can't set alias help for \""+cmd+"\", since it's a plugin or core command.");
 				return;
 			}
-			help = helpDB.getOne(cmd) || {};
+			help = alias.helpDB.getOne(cmd) || {};
 			if (aliasString) {
 				help.help = aliasString;
-				helpDB.saveOne(cmd, help);
+				alias.helpDB.saveOne(cmd, help);
 				irc.say(input.context, "Help for the \""+cmd+"\" alias has been set.");
 			} else {
 				irc.say(input.context, (help.help ? "[Help] Alias: "+config.command_prefix+cmd+" - "+
@@ -58,8 +47,8 @@ bot.command({
 				irc.say(input.context, "Which alias do you want to set the syntax for?");
 				return;
 			}
-			alias = aliasDB.getOne(cmd);
-			if (!alias) {
+			aliasEntry = alias.db.getOne(cmd);
+			if (!aliasEntry) {
 				irc.say(input.context, "There is no such alias.");
 				return;
 			}
@@ -67,14 +56,14 @@ bot.command({
 				irc.say(input.context, "You need to own the \""+cmd+"\" alias to set it's syntax.");
 				return;
 			}
-			if (aliasIsCommand(cmd)) {
+			if (bot.cmdExists(cmd)) {
 				irc.say(input.context, "You can't set alias syntax for \""+cmd+"\", since it's a plugin or core command.");
 				return;
 			}
-			help = helpDB.getOne(cmd) || {};
+			help = alias.helpDB.getOne(cmd) || {};
 			if (aliasString) {
 				help.syntax = aliasString;
-				helpDB.saveOne(cmd, help);
+				alias.helpDB.saveOne(cmd, help);
 				irc.say(input.context, "Syntax for the \""+cmd+"\" alias has been set.");
 			} else {
 				irc.say(input.context, (help.syntax ? "[Help] Alias syntax: "+config.command_prefix+cmd+" "+
@@ -86,8 +75,8 @@ bot.command({
 				irc.say(input.context, "Which alias do you want to set the minimum argument length for?");
 				return;
 			}
-			alias = aliasDB.getOne(cmd);
-			if (!alias) {
+			aliasEntry = alias.db.getOne(cmd);
+			if (!aliasEntry) {
 				irc.say(input.context, "There is no such alias.");
 				return;
 			}
@@ -95,12 +84,12 @@ bot.command({
 				irc.say(input.context, "You need to own the \""+cmd+"\" alias to set it's minimum argument length.");
 				return;
 			}
-			if (aliasIsCommand(cmd)) {
+			if (bot.cmdExists(cmd)) {
 				irc.say(input.context, "You can't set the minimum argument length for \""+cmd+
 					"\", since it's a plugin or core command.");
 				return;
 			}
-			help = helpDB.getOne(cmd) || {};
+			help = alias.helpDB.getOne(cmd) || {};
 			if (aliasString) {
 				aliasString = parseInt(aliasString, 10);
 				if (!Number.isInteger(aliasString) || aliasString < 0 || aliasString > 4) {
@@ -108,7 +97,7 @@ bot.command({
 					return;
 				}
 				help.arglen = aliasString;
-				helpDB.saveOne(cmd, help);
+				alias.helpDB.saveOne(cmd, help);
 				irc.say(input.context, "The minimum argument length for alias \""+cmd+"\" has been set.");
 			} else {
 				irc.say(input.context, (help.arglen !== undefined ? "[Help] The minimum argument length for "+
@@ -122,17 +111,17 @@ bot.command({
 					config.command_prefix+"alias add mitchslap action mitchslaps {args1}");
 				return;
 			}
-			alias = aliasDB.getOne(cmd);
-			if (alias && !perms.isOwner(input.user, "alias", cmd)) {
+			aliasEntry = alias.db.getOne(cmd);
+			if (aliasEntry && !perms.isOwner(input.user, "alias", cmd)) {
 				irc.say(input.context, "You need to own the \""+cmd+"\" alias to overwrite it.");
 				return;
 			}
-			if (aliasIsCommand(cmd)) {
+			if (bot.cmdExists(cmd)) {
 				irc.say(input.context, "The \""+cmd+"\" command is taken by a plugin or core function.");
 				return;
 			}
 			perms.Action(input.user, "owner add", "alias", cmd);
-			aliasDB.saveOne(cmd, aliasString);
+			alias.db.saveOne(cmd, aliasString);
 			irc.say(input.context, "Added :)");
 			break;
 		case "remove":
@@ -140,8 +129,8 @@ bot.command({
 				irc.say(input.context, "What should I remove?");
 				return;
 			}
-			alias = aliasDB.getOne(cmd);
-			if (!alias) {
+			aliasEntry = alias.db.getOne(cmd);
+			if (!aliasEntry) {
 				irc.say(input.context, "There is no such alias.");
 				return;
 			}
@@ -150,14 +139,14 @@ bot.command({
 					"\" alias to remove it. If you do own it, have you identified?");
 				return;
 			}
-			aliasDB.removeOne(cmd);
-			if (helpDB.getOne(cmd))
-				helpDB.removeOne(cmd);
+			alias.db.removeOne(cmd);
+			if (alias.helpDB.getOne(cmd))
+				alias.helpDB.removeOne(cmd);
 			perms.Action(input.user, "delete all", "alias", cmd);
 			irc.say(input.context, "Removed :)");
 			break;
 		case "list":
-			aliasList = aliasDB.getKeys().sort().join(", ") || "There are no aliases yet.";
+			aliasList = alias.db.getKeys().sort().join(", ") || "There are no aliases yet.";
 			irc.say(input.context, aliasList);
 			break;
 		case "info":
@@ -165,8 +154,8 @@ bot.command({
 				irc.say(input.context, "Which alias do you want info about?");
 				return;
 			}
-			alias = aliasDB.getOne(cmd);
-			irc.say(input.context, (alias ? "The alias \""+cmd+"\" contains: "+alias : "There is no such alias."));
+			aliasEntry = alias.db.getOne(cmd);
+			irc.say(input.context, (aliasEntry ? "The alias \""+cmd+"\" contains: "+aliasEntry : "There is no such alias."));
 			break;
 		default:
 			irc.say(input.context, bot.cmdHelp("alias", "syntax"));
@@ -180,33 +169,33 @@ bot.command({
 	help: "Allows you to add variables for use in aliases (only). Default variables are: "+
 		"{me} {from}, {channel}, {randThing}, {randNick}, {args*} (provided arguments), "+
 		"{args1} (first argument) - {variableName} for your custom vars.",
-	syntax: config.command_prefix+"var <add/remove/append/seppend/seprem/seprand/info> - "+
+	syntax: config.command_prefix+"let <add/remove/append/seppend/seprem/seprand/info> - "+
 		"try each command on it's own for further help.",
 	arglen: 1,
 	callback: function (input) {
-		var lcVarname, list, variable, varName, arr, varString, sep;
+		let lcVarname, list, variable, varName, arr, varString, sep;
 		input.user = input.nick+"!"+input.address;
 		varString = input.args.slice(2).join(" ").trim();
 		switch (input.args[0]) {
 		case "add":
 			if (!input.args[1] || !varString) {
 				irc.say(input.context, "[Help] Syntax: "+config.command_prefix+
-					"var add <variable> <entry> - Example: "+config.command_prefix+
-					"var add anime_list Steins;Gate, Hellsing Ultimate, Hyouka");
+					"let add <variable> <entry> - Example: "+config.command_prefix+
+					"let add anime_list Steins;Gate, Hellsing Ultimate, Hyouka");
 				return;
 			}
 			lcVarname = input.args[1].toLowerCase();
 			varName = "{" + lcVarname + "}";
-			variable = varDB.getOne(varName);
+			variable = alias.varDB.getOne(varName);
 			if (variable) {
 				if (perms.isOwner(input.user, "variable", lcVarname)) {
-					varDB.saveOne(varName, { handle: input.args[1], data: varString });
+					alias.varDB.saveOne(varName, { handle: input.args[1], data: varString });
 					irc.say(input.context, "Overwritten :S");
 				} else {
 					irc.say(input.context, "You need to own "+varName+" to overwrite it.");
 				}
 			} else {
-				varDB.saveOne(varName, { handle: input.args[1], data: varString });
+				alias.varDB.saveOne(varName, { handle: input.args[1], data: varString });
 				perms.Action(input.user, "owner add", "variable", lcVarname);
 				irc.say(input.context, "Created :)");
 			}
@@ -214,23 +203,23 @@ bot.command({
 		case "append":
 			if (!input.args[1] || !varString) {
 				irc.say(input.context, "[Help] Syntax: "+config.command_prefix+
-					"var append <variable> <entry> - Example: "+config.command_prefix+
-					"var append towatch ranma's DIY guide to unplugging a butt.");
+					"let append <variable> <entry> - Example: "+config.command_prefix+
+					"let append towatch ranma's DIY guide to unplugging a butt.");
 				return;
 			}
 			lcVarname = input.args[1].toLowerCase();
 			varName = "{" + lcVarname + "}";
-			variable = varDB.getOne(varName);
+			variable = alias.varDB.getOne(varName);
 			if (variable) {
 				if (perms.Check(input.user, "variable", lcVarname)) {
 					variable.data = variable.data+" "+varString;
-					varDB.saveOne(varName, variable);
+					alias.varDB.saveOne(varName, variable);
 					irc.say(input.context, "Added o7");
 				} else {
 					irc.say(input.context, "You don't have permission to do that.");
 				}
 			} else {
-				varDB.saveOne(varName, { handle: input.args[1], data: varString });
+				alias.varDB.saveOne(varName, { handle: input.args[1], data: varString });
 				irc.say(input.context, "Added o7");
 			}
 			break;
@@ -238,13 +227,13 @@ bot.command({
 			varString = input.args.slice(3).join(" ");
 			if (!input.args[1] || !input.args[2] || !varString) {
 				irc.say(input.context, "[Help] Syntax: "+config.command_prefix+
-					"var seppend <variable> <separator> <entry> - Example: "+config.command_prefix+
-					"var seppend towatch | Black Books");
+					"let seppend <variable> <separator> <entry> - Example: "+config.command_prefix+
+					"let seppend towatch | Black Books");
 				return;
 			}
 			lcVarname = input.args[1].toLowerCase();
 			varName = "{" + lcVarname + "}";
-			variable = varDB.getOne(varName);
+			variable = alias.varDB.getOne(varName);
 			if (variable) {
 				if (perms.Check(input.user, "variable", lcVarname)) {
 					if (variable.data.length > 0) {
@@ -252,13 +241,13 @@ bot.command({
 					} else {
 						variable.data = varString;
 					}
-					varDB.saveOne(varName, variable);
+					alias.varDB.saveOne(varName, variable);
 					irc.say(input.context, "Added o7");
 				} else {
 					irc.say(input.context, "You don't have permission to do that.");
 				}
 			} else {
-				varDB.saveOne(varName, { handle: input.args[1], data: varString });
+				alias.varDB.saveOne(varName, { handle: input.args[1], data: varString });
 				irc.say(input.context, "Added o7");
 			}
 			break;
@@ -266,13 +255,13 @@ bot.command({
 			varString = input.args.slice(3).join(" ");
 			if (!input.args[1] || !input.args[2] || !varString) {
 				irc.say(input.context, "[Help] Syntax: "+config.command_prefix+
-					"var seprem <varname> <separator> <entry> - Example: "+config.command_prefix+
-					"var seprem anime_list | Boku no Pico");
+					"let seprem <varname> <separator> <entry> - Example: "+config.command_prefix+
+					"let seprem anime_list | Boku no Pico");
 				return;
 			}
 			lcVarname = input.args[1].toLowerCase();
 			varName = "{" + lcVarname + "}";
-			variable = varDB.getOne(varName);
+			variable = alias.varDB.getOne(varName);
 			if (!variable) {
 				irc.say(input.context, "There is no \""+input.args[1]+"\" variable.");
 				return;
@@ -283,7 +272,7 @@ bot.command({
 			}
 			if (variable === varString) {
 				if (perms.isOwner(input.user, "variable", lcVarname)) {
-					varDB.removeOne(varName);
+					alias.varDB.removeOne(varName);
 					perms.Action(input.user, "delete all", "variable", lcVarname);
 					irc.say(input.context, "Removed o7");
 				} else {
@@ -298,7 +287,7 @@ bot.command({
 						variable.data = variable.data.filter(function (element) {
 							return (element.toLowerCase() !== varString.toLowerCase());
 						});
-						varDB.saveOne(varName, variable);
+						alias.varDB.saveOne(varName, variable);
 						irc.say(input.context, "Removed o7");
 						variable.data = variable.data.join(sep);
 						return;
@@ -311,11 +300,11 @@ bot.command({
 		case "seprand":
 			if (!input.args[1] || !input.args[2]) {
 				irc.say(input.context, "[Help] Syntax: "+config.command_prefix+
-					"var seprand <varname> <separator> - Example: "+config.command_prefix+"var seprand anime_list |");
+					"let seprand <varname> <separator> - Example: "+config.command_prefix+"let seprand anime_list |");
 				return;
 			}
 			varName = "{" + input.args[1].toLowerCase()+ "}";
-			variable = varDB.getOne(varName);
+			variable = alias.varDB.getOne(varName);
 			if (variable) {
 				if (input.args[2] === ",")
 					arr = variable.data.split(input.args[2]+" ");
@@ -329,18 +318,18 @@ bot.command({
 		case "remove":
 			if (!input.args[1]) {
 				irc.say(input.context, "[Help] Syntax: "+config.command_prefix+
-					"var remove <variable> - Example: "+config.command_prefix+"var remove anime_list");
+					"let remove <variable> - Example: "+config.command_prefix+"let remove anime_list");
 				return;
 			}
 			lcVarname = input.args[1].toLowerCase();
 			varName = "{"+lcVarname+"}";
-			variable = varDB.getOne(varName);
+			variable = alias.varDB.getOne(varName);
 			if (variable) {
 				if (!perms.isOwner(input.user, "variable", lcVarname)) {
 					irc.say(input.context, "You don't have permission to do that.");
 					return;
 				}
-				varDB.removeOne(varName);
+				alias.varDB.removeOne(varName);
 				perms.Action(input.user, "delete all", "variable", lcVarname);
 				irc.say(input.context, "Removed o7");
 			} else {
@@ -350,10 +339,10 @@ bot.command({
 		case "info":
 			if (!input.args[1]) {
 				irc.say(input.context, "[Help] Syntax: "+config.command_prefix+
-					"var info <variable> - Example: "+config.command_prefix+"var info anime_list");
+					"let info <variable> - Example: "+config.command_prefix+"let info anime_list");
 				return;
 			}
-			variable = varDB.getOne("{"+input.args[1].toLowerCase()+"}");
+			variable = alias.varDB.getOne("{"+input.args[1].toLowerCase()+"}");
 			if (variable)
 				irc.say(input.context, "Variable \""+input.args[1]+"\" contains: \""+variable.data+"\"");
 			else
@@ -361,8 +350,8 @@ bot.command({
 			break;
 		case "list":
 			list = [];
-			varDB.getKeys().sort().forEach(function (item) {
-				list.push(varDB.getOne(item).handle);
+			alias.varDB.getKeys().sort().forEach(function (item) {
+				list.push(alias.varDB.getOne(item).handle);
 			});
 			if (list.length)
 				irc.say(input.context, list.join(", "));
