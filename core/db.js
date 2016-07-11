1fbe67ed533b6cd30be1b34b8ad831e9c4f01cc4
Kaioshi <waderr@gmail.com>
2016-07-12 03:56:51 +0800

    [core/db] rewrite to use class inheritance (for some reason decreases mem use, o___o)

--- core/db.js
+++ core/db.js
@@ -1,262 +1,125 @@
 "use strict";
-let changed = {}, lastAccess = {}, clearCache = {}, dbCache = {}, isList = {};
+const dbCache = {};
 
-function saveCache() {
-	let now = Date.now(),
-		keys = Object.keys(dbCache);
-	for (let i = 0; i < keys.length; i++) {
-		let fn = keys[i];
-		if (changed[fn])
-			writeFile(fn, dbCache[fn]);
-		if (clearCache[fn]) {
-			if (!lastAccess[fn] || (now-lastAccess[fn]) >= 600000) {
-				lastAccess[fn] = null; dbCache[fn] = null;
-				delete lastAccess[fn]; // hasn't been touched in 10 minutes
-				delete dbCache[fn];
-			}
-		}
+class DB {
+	constructor(fn, defaultData, readFunc, writeFunc) {
+		this.fn = fn;
+		this._modified = false;
+		this._readFile = readFunc;
+		this._writeFile = writeFunc;
+		this._defaultData = defaultData;
+		this._loaded = false;
+		dbCache[this.fn] = this;
 	}
-	changed = null;
-	changed = {};
-}
-
-function cacheCheck(fn) {
-	lastAccess[fn] = Date.now();
-	if (!dbCache[fn])
-		dbCache[fn] = readFile(fn);
-}
-
-function writeFile(fn, data) {
-	let output;
-	if (isList[fn]) {
-		if (!Array.isArray(data)) {
-			logger.error("Tried to save non-array to "+fn);
+	static saveCache() { Object.keys(dbCache).forEach(db => dbCache[db].write()); }
+	get data() { this.read(); return this._data; }
+	set data(data) { this._data = data; }
+	getAll() { return this.data; }
+	saveAll(data) { this._modified = true; this._data = data; }
+	read() {
+		this._lastAccess = Date.now();
+		if (this._loaded)
+			return;
+		if (!fs.existsSync(this.fn) || fs.lstatSync(this.fn).size === 0) {
+			lib.fs.makePath(this.fn);
+			this._data = this._defaultData;
+			this._loaded = true;
 			return;
 		}
-		output = data.join("\n");
-	} else {
 		try {
-			output = JSON.stringify(data, null, 3);
-		} catch (e) {
-			logger.error("Couldn't JSON.stringify data for "+fn+": "+e, e);
+			this._data = this._readFile(this.fn);
+			this._loaded = true;
+		} catch (err) {
+			logger.error("Failed to read/parse "+this.fn+": "+err.message, err);
+		}
+	}
+	write() {
+		if (!this._modified) { // if DB wasn't accessed for >5 minutes, clear the data.
+			if (this._loaded && (!this._lastAccess || (Date.now()-this._lastAccess) >= 600000)) {
+				logger.debug(this.fn+"'s data wasn't accessed for over 10 minutes, clearing it.");
+				this._data = null;
+				this._loaded = false;
+			}
 			return;
 		}
-	}
-	try {
-		logger.info("Saving "+fn+" ...");
-		fs.writeFileSync(fn, output);
-	} catch (e) {
-		logger.error("Couldn't write to "+fn+": "+e, e);
-	}
-}
-
-function readFile(fn) {
-	if (!fs.existsSync(fn)) {
-		if (dbCache[fn]) // this
-			return dbCache[fn]; // shouldn't
-		dbCache[fn] = isList[fn] ? {} : []; // happen
-		changed[fn] = true;
-		logger.debug("readFile had to make "+fn+"'s data.");
-		return dbCache[fn];
-	}
-	if (isList[fn]) {
 		try {
-			if (fs.lstatSync(fn).size > 0)
-				return fs.readFileSync(fn).toString().split("\n");
-			else
-				return [];
-		} catch (e) {
-			logger.error("Couldn't read "+fn+": "+e, e.error);
-		}
-	} else {
-		try {
-			return JSON.parse(fs.readFileSync(fn).toString());
-		} catch (e) {
-			logger.error("Couldn't read or parse "+fn+": "+e, e.error);
+			logger.info("Saving "+this.fn+" ...");
+			this._writeFile(this.fn, this.data);
+			this._modified = false;
+		} catch (err) {
+			logger.error("Failed to write "+this.fn+": "+err.message, err);
 		}
 	}
 }
 
-class List {
-	constructor(options) {
-		this.fn = "data/"+options.filename+".txt";
-		isList[this.fn] = true;
-		clearCache[this.fn] = options.clearCache === undefined ? true : options.clearCache;
-		if (dbCache[this.fn] === undefined) {
-			if (!fs.existsSync(this.fn)) {
-				lib.fs.makePath(this.fn);
-				dbCache[this.fn] = [];
-				changed[this.fn] = true;
-			} else {
-				cacheCheck(this.fn);
+class Json extends DB {
+	constructor(fileName) {
+		super(fileName, {}, fn => JSON.parse(fs.readFileSync(fn).toString()),
+			(fn, data) => fs.writeFileSync(fn, JSON.stringify(data, null, 3)));
+	}
+	size() { return Object.keys(this.data).length; }
+	random() { return this.data[lib.randSelect(Object.keys(this.data))]; }
+	saveOne(handle, entry) { this._modified = true; this.data[handle] = entry; }
+	getOne(handle) { return this.data[handle] || false; }
+	hasOne(handle) { return this.data[handle] !== undefined; }
+	getKeys() { return Object.keys(this.data); }
+}
+
+class List extends DB {
+	constructor(fileName) {
+		super(fileName, [], fn => fs.readFileSync(fn).toString().split("\n"),
+			(fn, data) => fs.writeFileSync(fn, data.join("\n")));
+	}
+	size() { return this.data.length; }
+	random() { return lib.randSelect(this.data); }
+	saveOne(entry) { this._modified = true; this.data.push(entry); }
+	hasOne(entry, ignoreCase) { return this.indexOf(entry, ignoreCase) > -1; }
+	indexOf(entry, ignoreCase) {
+		if (ignoreCase) {
+			const match = entry.toLowerCase();
+			for (let i = 0; i < this.data.length; i++) {
+				if (this.data[i].toLowerCase() === match)
+					return i;
 			}
+			return -1;
 		}
+		return this.data.indexOf(entry);
 	}
-
-	size() {
-		cacheCheck(this.fn);
-		return dbCache[this.fn].length;
-	}
-
-	getAll() {
-		cacheCheck(this.fn);
-		return dbCache[this.fn];
-	}
-
 	getOne(entry, ignoreCase) {
-		cacheCheck(this.fn);
-		for (let i = 0; i < dbCache[this.fn].length; i++) {
-			if (ignoreCase) {
-				if (dbCache[this.fn][i].toLowerCase() === entry.toLowerCase())
-					return dbCache[this.fn][i];
-			} else if (dbCache[this.fn][i] === entry) {
-				return dbCache[this.fn][i];
-			}
+		const index = this.indexOf(entry, ignoreCase);
+		if (index > -1)
+			return this.data[index];
+	}
+	search(searchString, ignoreCase) { // returns an array of entries that contain match
+		let ret = [];
+		const match = ignoreCase ? searchString.toLowerCase() : searchString;
+		for (let i = 0; i < this.data.length; i++) {
+			const line = ignoreCase ? this.data[i].toLowerCase() : this.data[i];
+			if (line.indexOf(match) > -1)
+				ret.push(this.data[i]);
 		}
-		return false;
-	}
-
-	random() {
-		cacheCheck(this.fn);
-		return lib.randSelect(dbCache[this.fn]);
-	}
-	// returns an array of entries that contain "word"
-	search(word, ignoreCase) {
-		let i, lword, matches = [];
-		cacheCheck(this.fn);
-		lword = word.toLowerCase();
-		for (i = 0; i < dbCache[this.fn].length; i++) {
-			if (ignoreCase) {
-				if (dbCache[this.fn][i].indexOf(lword) > -1)
-					matches.push(dbCache[this.fn][i]);
-			} else if (dbCache[this.fn][i].indexOf(word) > -1) {
-				matches.push(dbCache[this.fn][i]);
-			}
-		}
-		return matches;
-	}
-
-	hasOne(entry, ignoreCase) {
-		let i, lentry;
-		cacheCheck(this.fn);
-		if (!ignoreCase)
-			return dbCache[this.fn].indexOf(entry) > -1;
-		lentry = entry.toLowerCase();
-		for (i = 0; i < dbCache[this.fn].length; i++) {
-			if (dbCache[this.fn][i].toLowerCase() === lentry)
-				return true;
-		}
-		return false;
-	}
-
-	saveAll(arr) {
-		cacheCheck(this.fn);
-		dbCache[this.fn] = arr;
-		changed[this.fn] = true;
-	}
-
-	saveOne(entry) {
-		cacheCheck(this.fn);
-		dbCache[this.fn].push(entry);
-		changed[this.fn] = true;
-	}
-
-	removeOne(entry) {
-		cacheCheck(this.fn);
-		for (let i = 0; i < dbCache[this.fn].length; i++) {
-			if (dbCache[this.fn][i] === entry) {
-				dbCache[this.fn].splice(i,1);
-				changed[this.fn] = true;
-				return;
-			}
-		}
+		return ret;
 	}
 }
 
-class Json {
-	constructor(options) {
-		this.fn = "data/"+options.filename+".json";
-		isList[this.fn] = false;
-		clearCache[this.fn] = options.clearCache === undefined ? true : options.clearCache;
-		if (dbCache[this.fn] === undefined) {
-			if (!fs.existsSync(this.fn)) {
-				lib.fs.makePath(this.fn);
-				dbCache[this.fn] = {};
-				changed[this.fn] = true;
-			} else {
-				cacheCheck(this.fn);
-			}
-		}
-	}
-
-	size() {
-		cacheCheck(this.fn);
-		return Object.keys(dbCache[this.fn]).length;
-	}
-
-	random() {
-		cacheCheck(this.fn);
-		return dbCache[this.fn][lib.randSelect(Object.keys(dbCache[this.fn]))];
-	}
-
-	getOne(entry) {
-		cacheCheck(this.fn);
-		return dbCache[this.fn][entry] || false;
-	}
-
-	saveOne(entry, data) {
-		cacheCheck(this.fn);
-		dbCache[this.fn][entry] = data;
-		changed[this.fn] = true;
-	}
-
-	removeOne(entry) {
-		cacheCheck(this.fn);
-		if (dbCache[this.fn][entry] !== undefined) {
-			dbCache[this.fn][entry] = null;
-			delete dbCache[this.fn][entry];
-			changed[this.fn] = true;
-		}
-	}
-
-	saveAll(data) {
-		cacheCheck(this.fn);
-		dbCache[this.fn] = null;
-		dbCache[this.fn] = data;
-		changed[this.fn] = true;
-	}
-
-	getAll() {
-		cacheCheck(this.fn);
-		return dbCache[this.fn];
-	}
-
-	getKeys() {
-		cacheCheck(this.fn);
-		return Object.keys(dbCache[this.fn]);
-	}
-
-	hasOne(entry) {
-		cacheCheck(this.fn);
-		return dbCache[this.fn].hasOwnProperty(entry);
-	}
-}
-
-setInterval(saveCache, 180000);
-
-// Save Cache on Exit
-process.on("closing", function () {
-	bot.emitEvent("closing");
-	saveCache();
-});
-
-process.on("SIGINT", function () {
-	bot.emitEvent("closing");
-	saveCache();
+setInterval(DB.saveCache, 300000);
+process.on("closing", DB.saveCache); // regular quit
+process.on("SIGINT", function () { // Ctrl-C - since we capture it we have to exit manually
+	DB.saveCache();
 	setTimeout(process.exit, 500);
 });
 
 plugin.declareGlobal("db", "DB", {
-	Json: Json,
-	List: List
+	Json: function (options) {
+		const fn = "data/"+options.filename+".json";
+		if (dbCache[fn])
+			return dbCache[fn];
+		return new Json(fn);
+	},
+	List: function (options) {
+		const fn = "data/"+options.filename+".txt";
+		if (dbCache[fn])
+			return dbCache[fn];
+		return new List(fn);
+	}
 });
