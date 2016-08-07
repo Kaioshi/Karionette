4f06c895b7e1c3a754acd1b2b75c42885406bc2f
Kaioshi <waderr@gmail.com>
2016-08-07 13:19:03 +0800

    [git] only announce changes on git pull

--- plugins/git.js
+++ plugins/git.js
@@ -1,42 +1,24 @@
 "use strict";
-let run = require("child_process").execFile,
-	gitDB = new DB.Json({filename: "gitannounce"});
+const [web, DB] = plugin.importMany("web", "DB"),
+	run = plugin.import("require")("child_process").execFile,
+	gitDB = new DB.Json({filename: "gitannounce2"});
 
-function checkGits() {
-	if (gitDB.size() === 0)
+function checkGits(target) {
+	if (!gitDB.hasOne("announce"))
 		return;
-	let aList = gitDB.getOne("announceList");
+	const aList = gitDB.getOne("announce");
 	if (!aList.length)
 		return;
-	web.atom2json("https://github.com/Kaioshi/Karionette/commits/master.atom").then(function (results) {
-		let latest = gitDB.getOne("latest") || [],
-			announce = [];
-		results.items.forEach(function (entry) {
-			if (latest.indexOf(entry.link) === -1) {
-				aList.forEach(function (target) {
-					announce.push([ "say", target, "git: "+lib.decode(entry.title)+" ~ "+entry.link ]);
-				});
-				latest.push(entry.link);
-			}
-		});
-		if (announce.length) {
-			irc.rated(announce);
-			gitDB.saveOne("latest", latest);
-		}
-	});
+	const latest = gitDB.getOne("latest");
+	web.json("https://api.github.com/repos/Kaioshi/Karionette/commits"+(latest ? "?since="+latest : ""))
+		.then(resp => {
+			if (resp[0].commit.author.date === latest)
+				return; // nothing new
+			gitDB.saveOne("latest", resp[0].commit.author.date);
+			if (target)
+				resp.forEach(commit => irc.say(target, commit.commit.message+" ~ "+commit.html_url));
+		}).catch(logger.error);
 }
-
-bot.event({
-	handle: "gitAnnouncerCheck",
-	event: "Ticker: 300s tick",
-	callback: checkGits
-});
-
-bot.event({ // check for updates when we start and joins are done
-	handle: "gitAnnouncerCheckOnStart",
-	event: "autojoinFinished",
-	callback: checkGits
-});
 
 bot.command({
 	command: "git",
@@ -45,48 +27,36 @@ bot.command({
 	admin: true,
 	arglen: 1,
 	callback: function (input) {
-		let changes, i, target, aList;
 		switch (input.args[0].toLowerCase()) {
 		case "fap": // quiet pull ;)
 			run("git", ["pull"], {}, function (error, stdout) {
-				stdout = stdout.split("\n");
 				let stats = "";
-				for (i = 0; i < stdout.length; i++) {
-					if (stdout[i].indexOf(" files changed") > -1 || stdout[i].indexOf(" file changed") > -1)
-						stats = stdout[i].trim();
-				}
-				if (stats)
+				stdout.split("\n").forEach(line => {
+					if (line.includes(" files changed") || line.includes(" file changed"))
+						stats = line.trim();
+				});
+				if (stats) {
 					irc.say(input.context, stats);
-				else
+					checkGits(); // no target since this needs to be quiet
+				} else {
 					irc.say(input.context, "Already up-to-date.");
+				}
 			});
 			break;
 		case "pull":
 			run("git", [ "pull" ], {}, function (error, stdout) {
-				stdout = stdout.split("\n");
-				for (i = 0, changes = []; i < stdout.length; i++) {
-					if (stdout[i][0] === " " && stdout[i][1] !== " ")
-						changes.push([ "say", input.context, stdout[i].trim() ]);
-				}
-				if (changes.length > 0)
-					irc.rated(changes);
-				else
+				let changes = false;
+				stdout.split("\n").forEach(line => {
+					if (line[0] === " " && line[1] !== " ") {
+						changes = true;
+						irc.say(input.context, line.trim());
+					}
+				});
+				if (!changes)
 					irc.say(input.context, "Already up-to-date.");
+				else
+					checkGits(input.context);
 			});
-			break;
-		case "announce": // toggle
-			target = input.context.toLowerCase();
-			aList = gitDB.getOne("announceList") || [];
-			if (lib.hasElement(aList, target)) {
-				aList = aList.filter(function (entry) { return entry !== target; });
-				gitDB.saveOne("announceList", aList);
-				irc.say(input.context, "Removed "+input.context+" from the git commit announcer.");
-			} else {
-				aList.push(target);
-				gitDB.saveOne("announceList", aList);
-				irc.say(input.context, "Added "+input.context+" to the git commit announcer.");
-				checkGits();
-			}
 			break;
 		default:
 			irc.say(input.context, bot.cmdHelp("git", "syntax"));
