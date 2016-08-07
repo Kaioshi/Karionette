78d7e7fe614c1849300a08b34511aa2155182f2a
Kaioshi <waderr@gmail.com>
2016-08-07 13:12:49 +0800

    [boot] neatify

--- boot.js
+++ boot.js
@@ -1,9 +1,5 @@
 "use strict";
-global.globals = {
-    memoryUsage: process.memoryUsage().rss,
-    startTime: Date.now(),
-    gc: global.gc
-};
+const globals = { startTime: Date.now() };
 
 process.on("uncaughtException", function caughtUncaughtException(err) {
 	if (plugin && plugin.sandbox && plugin.sandbox.logger)
@@ -12,22 +8,20 @@ process.on("uncaughtException", function caughtUncaughtException(err) {
 		console.error(err, err.stack);
 });
 
-const plugin = require("./plugin.js")(global.globals);
+const plugin = require("./plugin.js")(globals);
 plugin.loadCorePlugins([ // the order matters
 	"bot", "config", "logger", "lib", "ticker", "web", "db", "words", "ial", "alias", "ignore",
-	"logins", "perms", "bot-parse", "irc", "admin-events", "words-events", "core-events",
+	"logins", "perms", "irc", "admin-events", "words-events", "core-events",
 	"logins-events", "ial-events", "perms-events", "alias-events", "config-events"
 ]);
 plugin.loadOptionalPlugins();
 
-plugin.sandbox.irc.open({
-	server: plugin.sandbox.config.server,
-	port: plugin.sandbox.config.port,
-	nickname: plugin.sandbox.config.nick,
-	username: plugin.sandbox.config.username,
-	realname: plugin.sandbox.config.realname,
-	password: plugin.sandbox.config.password
-});
+if (plugin.sandbox.config.gc)
+	globals.gc = global.gc;
 
-if (plugin.sandbox.config.repl)
+plugin.sandbox.irc.connect();
+
+if (plugin.sandbox.config.repl) {
+	global.globals = globals;
 	require("repl").start({ prompt: "", ignoreUndefined: true });
+}
