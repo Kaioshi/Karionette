bcf22045c00ab471e8dd4c11be1e69cff1706262
Kaioshi <waderr@gmail.com>
2016-08-08 18:38:37 +0800

    [core/ticker] fluff

--- core/ticker.js
+++ core/ticker.js
@@ -5,14 +5,14 @@ const [setInterval, clearInterval] = plugin.importMany("setInterval", "clearInte
 class Ticker {
 	constructor() { this.tickers = Object.create(null); }
 	start(interval) {
-		if (this.tickers[interval])
+		if (this.tickers[interval] !== undefined)
 			return; // already running this ticker
-		this.tickers[interval] = setInterval(function() {
+		this.tickers[interval] = setInterval(function tick() {
 			bot.emitEvent("Ticker: "+interval+"s tick");
 		}, parseInt(interval, 10)*1000);
 	}
 	stop(interval) {
-		if (!this.tickers[interval])
+		if (this.tickers[interval] === undefined)
 			return;
 		this.tickers[interval].close();
 		clearInterval(this.tickers[interval]);
