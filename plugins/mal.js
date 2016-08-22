fdf8b2658c0dc9d283004460a7c8811d2ccb72de
Kaioshi <waderr@gmail.com>
2016-08-22 09:30:17 +0800

    [mal] use results.notFound

--- plugins/mal.js
+++ plugins/mal.js
@@ -14,11 +14,15 @@ function getGenres(genres) {
 }
 
 function doSearch(type, context, title, synopsis, google) {
-	let id, eps;
+	let id;
 	web.google("site:myanimelist.net/"+type+"/ "+title).then(function (results) {
-		id = new RegExp("http://myanimelist\\.net/"+type+"/([0-9]+)/?", "i").exec(results[0].url);
+		if (results.notFound) {
+			irc.say(context, web.notFound());
+			return;
+		}
+		id = new RegExp("http://myanimelist\\.net/"+type+"/([0-9]+)/?", "i").exec(results.items[0].url);
 		if (!id)
-			id = new RegExp("http://myanimelist\\.net/"+type+"\\.php\\?id=([0-9]+)", "i").exec(results[0].url);
+			id = new RegExp("http://myanimelist\\.net/"+type+"\\.php\\?id=([0-9]+)", "i").exec(results.items[0].url);
 		if (id)
 			id = id[1];
 		else {
@@ -27,29 +31,27 @@ function doSearch(type, context, title, synopsis, google) {
 			return;
 		}
 		if (google) {
-			irc.say(context, results[0].title.replace(" - MyAnimeList.net", "")+" - "+results[0].url+" - "+results[0].content);
+			irc.say(context, results[0].title.replace(" - MyAnimeList.net", "")+" - "+results.items[0].url+" - "+results.items[0].content);
 		} else {
 			return web.json("https://myanimelistrt.azurewebsites.net/2/"+type+"/"+id);
 		}
-	}).then(function (body) {
-		if (!body)
-			return;
-		if (body.error) {
-			irc.say(context, "The unofficial MAL API said: "+body.error+" - "+body.details);
+	}).then(function (media) {
+		if (!media)
+			return; // did the google option ^
+		if (media.error) {
+			irc.say(context, "The unofficial MAL API said: "+media.error+" - "+media.details);
 			return;
 		}
-		eps = "";
-		if (body.episodes) {
-			eps = " - "+body.episodes+" "+(parseInt(body.episodes, 10) > 1 ? "episodes" : "episode");
-		} else if (body.chapters) {
-			eps = " - "+body.chapters+" "+(parseInt(body.chapters, 10) > 1 ? "chapters" : "chapter");
+		let eps = "";
+		if (media.episodes) {
+			eps = " - "+media.episodes+" "+(parseInt(media.episodes, 10) > 1 ? "episodes" : "episode");
+		} else if (media.chapters) {
+			eps = " - "+media.chapters+" "+(parseInt(media.chapters, 10) > 1 ? "chapters" : "chapter");
 		}
-		irc.say(context, lib.decode(body.title)+" ~ Rank #"+body.rank+" ["+getGenres(body.genres)+"]"+
-			eps+" - "+body.status+" ~ http://myanimelist.net/"+type+"/"+id);
+		irc.say(context, lib.decode(media.title)+" ~ Rank #"+media.rank+" ["+getGenres(media.genres)+"]"+
+			eps+" - "+media.status+" ~ http://myanimelist.net/"+type+"/"+id);
 		if (synopsis)
-			irc.say(context, lib.stripHtml(lib.decode(body.synopsis)), 1);
-	}, function (error) {
-		irc.say(context, error.message);
+			irc.say(context, lib.stripHtml(lib.decode(media.synopsis)), 1);
 	}).catch(function (error) {
 		logger.error("Error in ;mal -> ", error);
 	});
