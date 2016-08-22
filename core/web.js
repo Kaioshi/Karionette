66ad0d856c4d0a73a0d8d43e4f91c485afc1b56c
Kaioshi <waderr@gmail.com>
2016-08-22 09:27:30 +0800

    [core/web] add a bunch of callback versions

--- core/web.js
+++ core/web.js
@@ -5,24 +5,30 @@ const lib = plugin.import("lib"),
 
 class Web {
 	constructor() {
+		this.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.63 Safari/537.36";
 		this._notFoundFeels = [
 			":(", "D:", ":V", ">:(", "Sorry!", ":\\",
 			"I'm not sorry.", "You only have yourself to blame.",
 			"Look what you did.", "For shame.",
 		];
 	}
+	notFound() { return `Couldn't find it. ${lib.randSelect(this._notFoundFeels)}`; }
 	fetch(uri, opts) {
-		return new Promise(function (resolve, reject) {
+		return new Promise((resolve, reject) => {
 			let args = [ "--compressed", "-sSL", encodeURI(uri) ];
 			if (!opts || !opts.nouseragent)
-				args.push(`-A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.63 Safari/537.36"`);
-			if (opts && opts.headers && Array.isArray(opts.headers) && opts.headers.length) {
-				opts.headers.forEach(function (h) {
-					args = args.concat([ "-H", h ]);
-				});
-			}
+				args.push(`-A "${this.userAgent}"`);
+			if (opts && opts.headers && Array.isArray(opts.headers) && opts.headers.length)
+				opts.headers.forEach(h => { args.push("-H"); args.push(h); })
 			opts = opts || { opts: { timeout: 15000 } };
 			run("curl", args, opts.opts, function (error, stdout, stderr) {
+				if (error) {
+					if (error.code === "ENOENT") {
+						logger.error("You need to install curl on the system.");
+						throw new Error("curl is not installed.");
+					}
+					throw new Error(error);
+				}
 				if (error && error.code === "ENOENT") {
 					logger.error("You need to install curl on the system.");
 					throw new Error("curl is not installed.");
@@ -37,12 +43,9 @@ class Web {
 	fetchAsync(uri, opts, callback) {
 		let args = [ "--compressed", "-sSL", encodeURI(uri) ];
 		if (!opts || !opts.nouseragent)
-			args.push(`-A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.63 Safari/537.36"`);
-		if (opts && opts.headers && Array.isArray(opts.headers) && opts.headers.length) {
-			opts.headers.forEach(function (h) {
-				args = args.concat([ "-H", h ]);
-			});
-		}
+			args.push(`-A "${this.userAgent}"`);
+		if (opts && opts.headers && Array.isArray(opts.headers) && opts.headers.length)
+			opts.headers.forEach(h => { args.push("-H"); args.push(h); });
 		opts = opts || { opts: { timeout: 15000 } };
 		run("curl", args, opts.opts, function (error, stdout, stderr) {
 			if (error && error.code === "ENOENT") {
@@ -54,23 +57,52 @@ class Web {
 			return callback(null, stdout);
 		});
 	}
+	jsonAsync(uri, opts, callback) {
+		this.fetchAsync(uri, opts, (err, stdout) => {
+			if (err)
+				return callback(err);
+			return callback(null, JSON.parse(stdout));
+		});
+	}
+	googleSearchAsync(uri, callback) {
+		this.jsonAsync(uri, null, (err, g) => {
+			if (err)
+				return callback(err);
+			if (g.error)
+				throw new Error("web.googleSearchAsync: "+g.error.message);
+			if (g.queries.request[0].totalResults === "0")
+				return callback(null, { notFound: true });
+			return callback(null, {
+				items: g.items.map(function (item) {
+					return {
+						title: lib.singleSpace(item.title),
+						url: item.link,
+						content: String(item.snippet).replace(/\x01|\n|\t|\r/g, "")
+					};
+				})
+			});
+		});
+	}
+	googleAsync(term, maxResults, callback) {
+		const uri = `https://www.googleapis.com/customsearch/v1?key=${config.api.googlesearch}&cx=002465313170830037306:5cfvjccuofo&num=${maxResults || 1}&prettyPrint=false&q=${term.trim()}`;
+		this.googleSearchAsync(uri, callback);
+	}
 	json(uri, opts) { return this.fetch(uri, opts).then(JSON.parse); }
 	googleSearch(uri) {
 		return this.json(uri).then(g => {
 			if (g.error)
-				throw new Error(`Google: ${g.error.message}`);
+				throw new Error("web.googleSearch: "+g.error.message);
 			if (g.queries.request[0].totalResults === "0")
-				throw new Error(`Couldn't find it. ${lib.randSelect(this._notFoundFeels)}`);
-			const ret = [];
-			for (let i = 0; i < g.items.length; i++) {
-				const item = g.items[i];
-				ret.push({
-					title: lib.singleSpace(item.title),
-					url: item.link,
-					content: String(item.snippet).replace(/\x01|\n|\t|\r/g, "")
-				});
+				return { notFound: true };
+			return {
+				items: g.items.map(function (item) {
+					return {
+						title: lib.singleSpace(item.title),
+						url: item.link,
+						content: String(item.snippet).replace(/\x01|\n|\t|\r/g, "")
+					};
+				})
 			}
-			return ret;
 		});
 	}
 	google(term, maxResults) {
@@ -84,25 +116,27 @@ class Web {
 	bing(term, maxResults) {
 		const uri = "https://api.datamarket.azure.com/Bing/SearchWeb/Web?$format=json&Query='"+
 			term+"'&Adult='Off'&Market='en-us'&$top="+(maxResults ? maxResults : 1),
-			auth = Buffer.from(config.api.bing+":"+config.api.bing).toString("base64");
-		return this.json(uri, { headers: { "Authorization": "Basic "+auth } }).then(b => {
+			auth = plugin.import("Buffer").from(config.api.bing+":"+config.api.bing).toString("base64");
+		return this.json(uri, { headers: [ "Authorization: Basic "+auth ] }).then(b => {
 			if (!b.d.results.length)
-				throw new Error("Couldn't find it. "+lib.randSelect(this._notFoundFeels));
-			let ret = [];
-			for (let i = 0; i < b.d.results.length; i++) {
-				let item = b.d.results[i];
-				ret.push({
-					title: lib.singleSpace(item.Title),
-					url: item.Url,
-					content: item.Description
-				});
-			}
-			return ret;
+				return { notFound: true };
+			return { items: b.d.results.map(function (item) { return { title: lib.singleSpace(item.Title), url: item.Url, content: item.Description }; }) };
+		});
+	}
+	bingAsync(term, maxResults, callback) {
+		const uri = "https://api.datamarket.azure.com/Bing/SearchWeb/Web?$format=json&Query='"+
+			term+"'&Adult='Off'&Market='en-us'&$top="+(maxResults ? maxResults : 1),
+			auth = plugin.import("Buffer").from(config.api.bing+":"+config.api.bing).toString("base64");
+		this.jsonAsync(uri, { headers: [ "Authorization: Basic "+auth ] }, (err, b) => {
+			if (err)
+				return callback(err);
+			if (!b.d.results.length)
+				return callback(null, { notFound: true });
+			return callback(null, { items: b.d.results.map(function (item) { return { title: lib.singleSpace(item.Title), url: item.Url, content: item.Description }; })})
 		});
 	}
 	youtubeSearch(searchTerm) {
-		const uri = "https://www.googleapis.com/youtube/v3/search?part=id&maxResults=1&q="+searchTerm+
-			"&safeSearch=none&type=video&fields=items&key="+config.api.youtube;
+		const uri = `https://www.googleapis.com/youtube/v3/search?part=id&maxResults=1&q=${searchTerm}&safeSearch=none&type=video&fields=items&key=${config.api.youtube}`;
 		return this.json(uri).then(resp => {
 			if (!resp.items.length)
 				throw new Error(searchTerm+" is not a thing on YouTube. "+lib.randSelect(this._notFoundFeels));
@@ -111,7 +145,7 @@ class Web {
 		});
 	}
 	youtubeByID(id) {
-		const uri = "https://www.googleapis.com/youtube/v3/videos?id="+id+"&key="+config.api.youtube+"&part=snippet,contentDetails,statistics";
+		const uri = `https://www.googleapis.com/youtube/v3/videos?id=${id}&key=${config.api.youtube}&part=snippet,contentDetails,statistics`;
 		return this.json(uri).then(yt => {
 			if (yt.error)
 				throw new Error(yt.error.errors[0]);
@@ -123,36 +157,6 @@ class Web {
 				duration: yt.items[0].contentDetails.duration.slice(2).toLowerCase(),
 				link: "https://youtube.com/watch?v="+yt.items[0].id
 			};
-		});
-	}
-	// TODO don't use this :D
-	atom2json(uri, handle) { // this is super buggy. so many corner cases.
-		const getXMLSection = function (line, section) {
-			return line.slice(line.indexOf("<"+section+">")+section.length+2, line.indexOf("</"+section+">")).trim();
-		};
-
-		return this.fetch(uri).then(function (source) {
-			if (source.indexOf("<entry>") === -1) {
-				if (handle)
-					return { name: handle, items: [] };
-				return { items: [] };
-			}
-			let atom = source.replace(/\n/g, "");
-			atom = atom.slice(atom.indexOf("<entry>"), atom.lastIndexOf("</entry>")+8);
-			atom = atom.replace(/<id>[^<]+<\/id>|<media:[^>]+\/>|<content type=\"html\">[^<]+<\/content>|<author>|<\/author>/g, "")
-				.replace(/<link ?((?!href).)*href=\"([^\"]+)\" ?\/>/g, "<link>$2</link>")
-				.replace(/<entry>/g, "").match(/\S+/g).join(" ").split("</entry>").slice(0,-1);
-			const ret = atom.map(function (entry) {
-				return {
-					title: getXMLSection(entry, "title"),
-					link: getXMLSection(entry, "link"),
-					name: getXMLSection(entry, "name"),
-					updated: getXMLSection(entry, "updated")
-				};
-			});
-			if (handle)
-				return { name: handle, items: ret };
-			return { items: ret };
 		});
 	}
 }
