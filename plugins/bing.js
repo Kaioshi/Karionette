8992089b9233953b385fddb553d1fd43d45f0250
Kaioshi <waderr@gmail.com>
2016-08-22 09:29:23 +0800

    [bing] unbreak, switch to generator

--- plugins/bing.js
+++ plugins/bing.js
@@ -6,21 +6,26 @@ bot.command({
 	help: "Bing searches things.",
 	syntax: config.command_prefix+"bing Oh god why am I using bing",
 	arglen: 1,
-	callback: function (input) {
+	callback: function bing(input) {
 		if (!config.api.bing) {
 			irc.say(input.context, "You need a bing api key in the config. "+
 				"Get one at https://datamarket.azure.com/dataset/bing/searchweb");
 			return;
 		}
-		web.bing(input.data).then(function (results) {
-			if (config.bing_format) {
-				results[0].b = "\x02";
-				irc.say(input.context, lib.formatOutput(config.bing_format, results[0]), 1);
-			} else {
-				irc.say(input.context, lib.formatOutput("{title} ~ {url} ~ {content}", results[0]), 1);
+		lib.runCallback(function *main(cb) { try {
+			const results = yield web.bingAsync(input.data, 1, cb);
+			if (results.notFound) {
+				irc.say(input.context, web.notFound());
+				return;
 			}
-		}, function (error) {
-			irc.say(input.context, error.message);
-		});
+			if (config.bing_format) {
+				results.items[0].b = "\x02";
+				irc.say(input.context, lib.formatOutput(config.bing_format, results.items[0]), false, 1);
+			} else {
+				irc.say(input.context, `${results.items[0].title} ~ ${results.items[0].url} ~ ${results.items[0].content}`, false, 1);
+			}
+		} catch (err) {
+			logger.error(`;bing - ${err.message}`, err);
+		}});
 	}
 });
