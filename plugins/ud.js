b8b07172986526abdef17b0b2764de97a0dcab8a
Kaioshi <waderr@gmail.com>
2016-08-22 09:31:22 +0800

    [ud] complete uglification

--- plugins/ud.js
+++ plugins/ud.js
@@ -10,11 +10,12 @@ bot.command({
 	arglen: 1,
 	callback: function ud(input) {
 		lib.runCallback(function *main(cb) { try {
-			const json = JSON.parse(yield web.fetchAsync("http://api.urbandictionary.com/v0/define?term="+input.data, null, cb));
-			const results = json.list.filter(entry => entry.thumbs_down > entry.thumbs_up || entry.thumbs_down > (entry.thumbs_up/2))
-				.sort((a, b) => { if (a.thumbs_up > b.thumbs_up) return 1; if (a.thumbs_up < b.thumbs_up) return -1; return 0; })
-				.map(entry => lib.singleSpace(entry.definition.replace(/\r|\n/g, " "))).join(" -- ") || "Pantsu.";
-			irc.say(input.context, results, true, 2);
+			irc.say(input.context,
+				JSON.parse(yield web.fetchAsync("http://api.urbandictionary.com/v0/define?term="+input.data, null, cb)).list
+					.filter(entry => entry.thumbs_down > entry.thumbs_up || entry.thumbs_down > (entry.thumbs_up/2))
+					.sort((a, b) => { if (a.thumbs_up > b.thumbs_up) return 1; if (a.thumbs_up < b.thumbs_up) return -1; return 0; })
+					.map(entry => lib.singleSpace(entry.definition.replace(/\r|\n/g, " "))).join(" -- ") || web.notFound(),
+				true, 2);
 		} catch (error) {
 			logger.error("ud - "+error.message, error);
 		}});
