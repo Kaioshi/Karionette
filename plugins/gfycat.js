c2700f8ad111f523f166be6d774902fb418a7955
Kaioshi <waderr@gmail.com>
2017-04-27 16:14:10 +0800

    [plugins/gfycat] make gfycat more robust

--- plugins/gfycat.js
+++ plugins/gfycat.js
@@ -9,15 +9,18 @@ bot.command({
 	syntax: config.command_prefix+"gfy <url> - Example: "+
 		config.command_prefix+"gfy http://i.imgur.com/N2FEP.gif",
 	arglen: 1,
-	callback: function (input) {
-		web.json("http://upload.gfycat.com/transcode?fetchUrl="+input.args[0]).then(function (gfy) {
-			if (gfy.error)
-				irc.say(input.context, "Gfycat said \""+gfy.error+"\"");
-			else
-				irc.say(input.context, "http://gfycat.com/"+gfy.gfyname+" - Shrunk by "+
-					Math.round((gfy.gifSize - gfy.gfysize)/1024)+" KiB - Framerate: "+gfy.frameRate+".");
-		}, function (error) {
-			irc.say(input.context, error.message);
-		});
+	callback: async function (input) {
+		try { // extended timeout time since it can take a while - 2 mins
+			const gfy = await web.json(`http://upload.gfycat.com/transcode?fetchUrl=${input.args[0]}`, { opt: { timeout: 120000 }});
+			if (gfy.error || (gfy.task && gfy.task === "error")) {
+				irc.say(input.context, `Couldn't gfycat that. :(`);
+			} else {
+				const shrunk = gfy.gifSize && gfy.gfysize ? ` - Shrunk by ${Math.round((gfy.gifSize - gfy.gfysize)/1024)} KiB` : "";
+				const framerate = gfy.frameRate ? ` - Framerate: ${gfy.frameRate}` : "";
+				irc.say(input.context, `http://gfycat.com/${gfy.gfyname}${shrunk}${framerate}`);
+			}
+		} catch (err) {
+			logger.error("Couldn't gfy that", err);
+		}
 	}
 });
