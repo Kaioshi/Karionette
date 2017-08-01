ff837e103856ad79d0cf2e5e38cb7b8b5a172655
Kaioshi <waderr@gmail.com>
2017-08-01 14:01:36 +0800

    [btc] new API

--- plugins/btc.js
+++ plugins/btc.js
@@ -3,30 +3,26 @@
 const [web, lib] = plugin.importMany("web", "lib");
 
 function currencyURL(code) {
-	return `https://api.bitcoinaverage.com/exchanges/${code}`;
+	return `https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC${code}`;
 }
 
-let KEYS = ["AUD", "BRL", "CAD", "CHF", "CNY", "EUR", "GBP", "IDR", "ILS", "MXN", "NOK", "NZD", "PLN", "RON", "RUB", "SEK", "SGD", "USD", "ZAR"];
+const KEYS = ["AUD","BRL","CAD","CNY","CZK","EUR","GBP","IDR","ILS","JPY","MXN","MYR","NGN","NZD","PLN","RUB","SEK","SGD","TRY","USD","ZAR","AED",
+	"AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTC","BTN","BWP","BYN",
+	"BZD","CAD","CDF","CHF","CLF","CLP","CNH","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP",
+	"GBP","GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JEP","JMD","JOD",
+	"JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRO",
+	"MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB",
+	"RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STD","SVC","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TWD",
+	"TZS","UAH","UGX","USD","UYU","UZS","VEF","VND","VUV","WST","XAF","XAG","XAU","XCD","XDR","XOF","XPD","XPF","XPT","YER","ZAR","ZMW","ZWL"];
 
 let CURRENCY = KEYS.reduce((acc, code) => { acc[code] = currencyURL(code); return acc; }, {});
-
-function trimNum(n) {
-	let i;
-	n = n.toString();
-	i = n.indexOf(".");
-	if (i === -1)
-		return n;
-	if (i === -1 || n.slice(i).length < 2)
-		return n;
-	return n.slice(0, i+3);
-}
 
 bot.command({
 	command: "btc",
 	help: "Retrieves high and low of bitcoin value. Defaults to USD if no argument is given",
 	syntax: `${config.command_prefix}btc [<currency code>] - Example: ${config.command_prefix}btc AUD`,
 	callback: function (input) {
-		let provider, rate, cur, time, avg, high, low;
+		let cur;
 		if (input.args && input.args[0] !== undefined)
 			cur = input.args[0].toUpperCase();
 		else
@@ -36,31 +32,10 @@ bot.command({
 			return;
 		}
 		web.json(CURRENCY[cur]).then(function (btc) {
-			if (btc.timestamp) {
-				time = `data fetched ${lib.duration(new Date(btc.timestamp))} ago.`;
-				delete btc.timestamp;
-			} else {
-				time = "";
-			}
-			avg = 0;
-			high = { val: 0 };
-			low = { val: 0 };
-			for (provider in btc) {
-				if (btc.hasOwnProperty(provider)) {
-					rate = btc[provider].rates.last;
-					avg += rate;
-					if (rate > high.val || !high.val) {
-						high.val = rate;
-						high.from = btc[provider].display_name;
-					}
-					if (rate < low.val || !low.val) {
-						low.val = rate;
-						low.from = btc[provider].display_name;
-					}
-				}
-			}
-			avg = avg / Object.keys(btc).length;
-			irc.say(input.context, `Bitcoin values: [${cur}] Average: ${trimNum(avg)}, high: ${high.val} [${high.from}], low: ${low.val} [${low.from}] (${time})`);
+			irc.say(input.context, `BTC prices in ${cur} - high: ${btc.high}, low: ${btc.low} - average for the day: ${btc.averages.day}, week: ${btc.averages.week}, month: ${btc.averages.month}`);
+		}).catch(err => {
+			irc.say(input.context, "Somethin' done broke. Whine at Kaioshi.");
+			logger.error(";btc failed", err);
 		});
 	}
 });
